66ceabe7352fe740acb79562d4f18235d5d6eb68
zhiyilee <zhiyi514@gmail.com>
2020-11-25 11:36:25 -0600

    added types

---
+++ index.d.ts
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import {
+    OText, 
+    OButton, 
+    OInput, 
+    OSegment, 
+    ODropDown, 
+    OIcon, 
+    OIconText, 
+    OCheckbox, 
+    OIconButton, 
+    OTextarea,
+    OToggle
+} from './src/components/shared'
+
+export type OText = React.Component<{}>;
+export type OButton = React.Component<{}>;
+export type OInput = React.Component<{}>;
