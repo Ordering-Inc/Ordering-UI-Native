451d828524febbcf1da188ffec4269cd1f3563de
zhiyilee <zhiyi514@gmail.com>
2020-11-25 11:43:38 -0600

    updated types

--- index.d.ts
+++ index.d.ts
@@ -13,6 +13,28 @@ import {
     OToggle
 } from './src/components/shared'
 
-export type OText = React.Component<{}>;
-export type OButton = React.Component<{}>;
-export type OInput = React.Component<{}>;
+declare type OText = React.Component<{}>;
+declare type OButton = React.Component<{}>;
+declare type OInput = React.Component<{}>;
+declare type OSegment = React.Component<{}>;
+declare type OIcon = React.Component<{}>;
+declare type ODropDown = React.Component<{}>;
+declare type OIconText = React.Component<{}>;
+declare type OCheckbox = React.Component<{}>;
+declare type OIconButton = React.Component<{}>;
+declare type OTextarea = React.Component<{}>;
+declare type OToggle = React.Component<{}>;
+
+export { 
+    OText, 
+    OButton, 
+    OInput, 
+    OSegment, 
+    ODropDown, 
+    OIcon, 
+    OIconText, 
+    OCheckbox, 
+    OIconButton, 
+    OTextarea,
+    OToggle
+}
