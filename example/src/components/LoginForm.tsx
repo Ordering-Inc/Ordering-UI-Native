6691221f03464d4919ebf30596c563706e190658
carlos quiroz <carloseqv29596@gmail.com>
2021-01-06 16:50:46 -0400

    [FEAT] use loginForm components from ordering-components lib

--- example/src/components/LoginForm.tsx
+++ example/src/components/LoginForm.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
-import { ViewStyle } from 'react-native';
+import { ViewStyle, Text } from 'react-native';
 import styled from 'styled-components/native';
+import { LoginForm as LoginFormController } from 'ordering-components/Native'
+
 import { IMAGES, STORAGE_KEY } from '../config/constants';
 import ApiProvider from '../providers/ApiProvider';
 import { ToastType, useToast } from '../providers/ToastProvider';
@@ -35,19 +37,16 @@ export interface ViewInterface {
 }
 
 export const Wrapper = styled.View<ViewInterface>`
-    background-color: ${ props => props.backgroundColor };
-    border: ${ props => props.border };
+    background-color: ${(props: any) => props.backgroundColor};
+    border: ${(props: any) => props.border};
     border-radius: 20px;
     border-bottom-right-radius: 0;
     border-bottom-left-radius: 0;
 `
 
-const LoginForm = (props: ViewInterface) => {
-
+const LoginFormUI = (props: ViewInterface) => {
     const ordering = ApiProvider();
-    
     const { showToast } = useToast();
-
     const auth = {
         email: '',
         password: ''
@@ -94,7 +93,7 @@ const LoginForm = (props: ViewInterface) => {
             console.log(err)
         });
     }
-    
+
     let title, sub_title, reg_button
     if (props.title) {
         title = <OText style={{fontSize: 24, color: 'white'}}>{props.title}</OText>
@@ -115,50 +114,55 @@ const LoginForm = (props: ViewInterface) => {
         )
     }
     return (
-        <Wrapper 
-            backgroundColor={props.backgroundColor} 
-            borderRadius={ props.borderRadius || '0px' }
-            style={props.wrapperStyle}
-            border={ props.border }>
-            <Spinner 
-                visible={is_loading}
-            />
-            { title }
-            { sub_title }
-            <OInput 
-                placeholder={'Email'}
-                style={{marginBottom: 10}}
-                icon={IMAGES.email}
-                value={email}
-                onChange={(e:any) => onChangEmail(e)}
-                />
-            <OInput 
-                isSecured={true}
-                placeholder={'Password'}
-                style={{marginBottom: 25}}
-                icon={IMAGES.lock}
-                value={password}
-                onChange={(p:any) => onChangPassword(p)}
-                />
-            <OButton 
-                onClick={onLogin}
-                text={props.loginButtonText}
-                bgColor={props.loginButtonBackground}
-                borderColor={props.loginButtonBackground}
-                textStyle={{color: 'white'}}
-                style={{marginBottom: 14}}
-                />
-            { reg_button }
-            <OButton 
-                onClick={props.onForgot}
-                text={props.forgotButtonText}
-                bgColor={colors.clear}
-                borderColor={colors.clear}
-                textStyle={{color: 'white'}}
-                imgRightSrc={null}
-            />
-        </Wrapper>
+      <Wrapper
+        backgroundColor={props.backgroundColor}
+        borderRadius={ props.borderRadius || '0px' }
+        style={props.wrapperStyle}
+        border={ props.border }
+      >
+        <Spinner visible={is_loading} />
+        { title }
+        { sub_title }
+        <OInput
+          placeholder={'Email'}
+          style={{marginBottom: 10}}
+          icon={IMAGES.email}
+          value={email}
+          onChange={(e:any) => onChangEmail(e)}
+        />
+        <OInput
+          isSecured={true}
+          placeholder={'Password'}
+          style={{marginBottom: 25}}
+          icon={IMAGES.lock}
+          value={password}
+          onChange={(p:any) => onChangPassword(p)}
+        />
+        <OButton
+          onClick={onLogin}
+          text={props.loginButtonText}
+          bgColor={props.loginButtonBackground}
+          borderColor={props.loginButtonBackground}
+          textStyle={{color: 'white'}}
+          style={{marginBottom: 14}}
+        />
+        { reg_button }
+        <OButton
+          onClick={props.onForgot}
+          text={props.forgotButtonText}
+          bgColor={colors.clear}
+          borderColor={colors.clear}
+          textStyle={{color: 'white'}}
+          imgRightSrc={null}
+        />
+      </Wrapper>
     );
 };
 
-export default LoginForm;
+export const LoginForm = (props: any) => {
+  const loginProps = {
+    ...props,
+    UIComponent: LoginFormUI
+  }
+  return <LoginFormController {...loginProps} />
+}
--- example/src/pages/Login.tsx
+++ example/src/pages/Login.tsx
@@ -1,10 +1,8 @@
 import * as React from 'react';
 import { Platform, View, Text } from "react-native";
 import styled from 'styled-components/native'
-import LoginForm from '../components/LoginForm';
+import { LoginForm } from '../components/LoginForm';
 import { colors } from '../theme';
-//@ts-ignore
-// import { LoginForm } from 'ordering-components/_modules/native';
 
 const BgWrapper = styled.ImageBackground`
   flex: 1;
@@ -18,51 +16,39 @@ const KeyboardView = styled.KeyboardAvoidingView`
   flex-grow: 1;
 `
 
-export const bgImage = require('../assets/images/home_bg.png'); 
+export const bgImage = require('../assets/images/home_bg.png');
 export const bdRadius = { topRight: '20px', topLeft: '20px', bottomRigt: '0px', bottomLeft: '0px'}
 
-export const TestForm = () => {
-    return(
-        <View style={{backgroundColor: 'red', height: 100, flex: 1}}>
-            <Text>{'Ok this is test'}</Text>
-        </View>
-    )
-}
-
 const Login = ({ navigation }: any) => {
-    
-    const register = () => {
-        alert('This is test register button');
-    }
-    const forgot = () => {
-        navigation.navigate('Forgot');
-    }
+  const register = () => {
+    alert('This is test register button');
+  }
+  const forgot = () => {
+    navigation.navigate('Forgot');
+  }
 
-    return (
-        <BgWrapper
-            source={ bgImage }>
-
-            <KeyboardView enabled behavior={ Platform.OS === 'ios'? "padding" : "height"}>
-                <LoginWrapper>
-                    {/* <LoginForm UIcomponent={TestForm} /> */}
-                    <LoginForm
-                        navigation={navigation}
-                        title="Welcome to login!"
-                        subTitle="Let's start your delivery orders!"
-                        wrapperStyle={{padding: 20}}
-                        border="1px solid"
-                        borderRadius="20px"
-                        backgroundColor={colors.secondary}
-                        loginButtonText="Login"
-                        registerButtonText="Register"
-                        forgotButtonText="Forgot password?"
-                        onForgot={forgot}
-                        loginButtonBackground={colors.primary}/>
-                </LoginWrapper>
-            </KeyboardView>
-
-        </BgWrapper>
-    )
+  return (
+    <BgWrapper source={ bgImage }>
+      <KeyboardView enabled behavior={ Platform.OS === 'ios'? "padding" : "height"}>
+        <LoginWrapper>
+          <LoginForm
+            navigation={navigation}
+            title="Welcome to login!"
+            subTitle="Let's start your delivery orders!"
+            backgroundColor={colors.secondary}
+            borderRadius="20px"
+            wrapperStyle={{padding: 20}}
+            border="1px solid"
+            loginButtonText="Login"
+            loginButtonBackground={colors.primary}
+            onForgot={forgot}
+            forgotButtonText="Forgot password?"
+            registerButtonText="Register"
+          />
+        </LoginWrapper>
+      </KeyboardView>
+    </BgWrapper>
+  )
 }
 
 export default Login;
