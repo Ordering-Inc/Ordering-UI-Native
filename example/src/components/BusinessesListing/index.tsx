ee5b8b7d0285dc2ca8ef76ee14cf324907a70206
Jesus Perez <jesus@ordering.co>
2021-03-26 02:41:27 -0400

    [FIX] loader of businessList

--- example/src/components/BusinessesListing/index.tsx
+++ example/src/components/BusinessesListing/index.tsx
@@ -113,19 +113,23 @@ const BusinessesListingUI = (props: BusinessesListingParams) => {
         )
       }
       {businessesList.loading && (
-        <Placeholder Animation={Fade}>
-          <View style={{ width: '100%' }}>
-            <PlaceholderLine height={200} style={{ marginBottom: 20, borderRadius: 25 }} />
-            <View style={{ paddingHorizontal: 10 }}>
-              <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-                <PlaceholderLine height={25} width={40} style={{ marginBottom: 10 }} />
-                <PlaceholderLine height={25} width={20} style={{ marginBottom: 10 }} />
+        <>
+          {[...Array(paginationProps.nextPageItems ? paginationProps.nextPageItems : 8).keys()].map((item, i) => (
+            <Placeholder Animation={Fade} key={i} style={{marginBottom: 20}}>
+              <View style={{ width: '100%' }}>
+                <PlaceholderLine height={200} style={{ marginBottom: 20, borderRadius: 25 }} />
+                <View style={{ paddingHorizontal: 10 }}>
+                  <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+                    <PlaceholderLine height={25} width={40} style={{ marginBottom: 10 }} />
+                    <PlaceholderLine height={25} width={20} style={{ marginBottom: 10 }} />
+                  </View>
+                  <PlaceholderLine height={20} width={30} style={{ marginBottom: 10 }} />
+                  <PlaceholderLine height={20} width={80} style={{ marginBottom: 10 }} />
+                </View>
               </View>
-              <PlaceholderLine height={20} width={30} style={{ marginBottom: 10 }} />
-              <PlaceholderLine height={20} width={80} style={{ marginBottom: 10 }} />
-            </View>
-          </View>
-        </Placeholder>
+            </Placeholder>
+          ))}
+        </>
       )}
       {
         !businessesList.loading && businessesList.businesses?.map((business: any) => (
