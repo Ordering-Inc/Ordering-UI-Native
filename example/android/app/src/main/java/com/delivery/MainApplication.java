617e742f40074d0de49d656c79d7979ca85f0818
carlosq-ordering <carlos.q@ordering.co>
2021-04-06 11:50:55 -0400

    update files

--- example/android/app/src/main/java/com/delivery/MainApplication.java
+++ example/android/app/src/main/java/com/delivery/MainApplication.java
@@ -5,7 +5,6 @@ import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.gettipsi.stripe.StripeReactPackage;
-import com.fitpassu.stripepayments.StripePaymentsPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
