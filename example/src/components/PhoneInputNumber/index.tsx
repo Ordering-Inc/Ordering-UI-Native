5428de8058f9bde5db7240508728db91db3c969d
Jesus Perez <jesus@ordering.co>
2021-04-05 23:11:19 -0400

    [FIX] input can only save numbers without special characters

--- example/src/components/PhoneInputNumber/index.tsx
+++ example/src/components/PhoneInputNumber/index.tsx
@@ -20,13 +20,20 @@ export const PhoneInputNumber = (props: PhoneInputParams) => {
   const phoneInput = useRef<PhoneInput>(null);
   const [userphoneNumber, setUserphoneNumber] = useState('');
 
+  const handleChangeNumber = (number) => {
+    setUserphoneNumber(number)
+  }
+
   useEffect(() => {
     if((defaultValue && userphoneNumber) || defaultValue === undefined){
       if (userphoneNumber) {
         const checkValid = phoneInput.current?.isValidNumber(userphoneNumber);
         const callingCode = phoneInput.current?.getCallingCode();
         const formattedNumber = phoneInput.current?.getNumberAfterPossiblyEliminatingZero();
-        if (!checkValid && formattedNumber?.number) {
+        const regex = /^[0-9]*$/
+        const cellphone = userphoneNumber.slice(0, 0) + userphoneNumber.slice(1, userphoneNumber.length)
+        const validNumber = regex.test(cellphone)
+        if ((!checkValid && formattedNumber?.number) || !validNumber) {
           handleData && handleData({
             ...data,
             error: t('INVALID_ERROR_PHONE_NUMBER', 'The Phone Number field is invalid')
@@ -59,7 +66,7 @@ export const PhoneInputNumber = (props: PhoneInputParams) => {
         ref={phoneInput}
         defaultValue={userphoneNumber || defaultValue}
         defaultCode={configs?.default_country_code?.value}
-        onChangeFormattedText={(text) => setUserphoneNumber(text)}
+        onChangeFormattedText={(text) => handleChangeNumber(text)}
         withDarkTheme
         countryPickerProps={{withAlphaFilter:true}}
         textContainerStyle={style.input}
