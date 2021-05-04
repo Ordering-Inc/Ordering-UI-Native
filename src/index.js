2e5746065cbae37a2c9bdde3b0c1b598e4b0dd72
Jesus Perez <jesus@ordering.co>
2021-05-04 17:55:09 -0400

    build

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import {
+  OText,
+  OButton,
+  OInput,
+  OSegment,
+  ODropDown,
+  OIcon,
+  OIconText,
+  OCheckbox,
+  OIconButton,
+  OTextarea,
+  OToggle,
+} from './components/shared';
+import LoginForm from './components/LoginForm';
+
+export {
+  OText,
+  OButton,
+  OInput,
+  OSegment,
+  ODropDown,
+  OIcon,
+  OIconText,
+  OCheckbox,
+  OIconButton,
+  OTextarea,
+  OToggle,
+  LoginForm,
+};
