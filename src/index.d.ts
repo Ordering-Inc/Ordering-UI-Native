a7848014cf54ddc40f321d0b41fa7e3cd5c2cf79
Jesus Perez <jesus@ordering.co>
2021-05-16 22:21:05 -0400

    move index.d.ts

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,15 +1,15 @@
 import * as React from 'react';
-import { OToggleProps } from './src/components/shared/OToggle';
-import { OTextAreaProps } from './src/components/shared/OTextarea';
-import { OIconButtonProps } from './src/components/shared/OIconButton';
-import { OCheckboxProps } from './src/components/shared/OCheckbox';
-import { ODropDownProps } from './src/components/shared/ODropDown';
-import { OSegmentProps } from './src/components/shared/OSegment';
-import { OInputProps } from './src/components/shared/OInput';
-import { OButtonProps } from './src/components/shared/OButton';
-import { OTextProps } from './src/components/shared/OText';
-import { OIconProps } from './src/components/shared/OIcon';
-import { LoginParams, PhoneInputParams } from './src/types';
+import { OToggleProps } from './components/shared/OToggle';
+import { OTextAreaProps } from './components/shared/OTextarea';
+import { OIconButtonProps } from './components/shared/OIconButton';
+import { OCheckboxProps } from './components/shared/OCheckbox';
+import { ODropDownProps } from './components/shared/ODropDown';
+import { OSegmentProps } from './components/shared/OSegment';
+import { OInputProps } from './components/shared/OInput';
+import { OButtonProps } from './components/shared/OButton';
+import { OTextProps } from './components/shared/OText';
+import { OIconProps } from './components/shared/OIcon';
+import { LoginParams, PhoneInputParams } from './types';
 
 declare class OText extends React.Component<OTextProps> { }
 declare class OButton extends React.Component<OButtonProps> { }
