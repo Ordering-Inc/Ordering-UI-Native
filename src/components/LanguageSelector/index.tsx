92d5eb7ad2b3b6bfe67e476060ac885491a8c123
Jesus Perez <jesus@ordering.co>
2021-05-16 23:18:30 -0400

    [update] components

---
+++ src/components/LanguageSelector/index.tsx
@@ -0,0 +1,87 @@
+import React from 'react'
+import { LanguageSelector as LanguageSelectorController, useOrder } from 'ordering-components/native'
+import { Platform, StyleSheet } from 'react-native'
+
+import RNPickerSelect from 'react-native-picker-select'
+import { Container } from './styles'
+import { colors } from '../../theme.json'
+import { LanguageSelectorParams } from '../../types'
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons'
+
+const LanguageSelectorUI = (props: LanguageSelectorParams) => {
+
+  const [orderState] = useOrder()
+
+  const {
+    languagesState,
+    currentLanguage,
+    handleChangeLanguage,
+  } = props
+
+  const _languages = languagesState?.languages?.map((language: any) => {
+    return {
+      value: language?.code,
+      label: language?.name,
+      inputLabel: language?.code.toUpperCase()
+    }
+  })
+  _languages && _languages.sort((a: any, b: any) =>
+    (a.content > b.content) ? 1 : ((b.content > a.content) ? -1 : 0)
+  )
+  return (
+    <Container>
+      {languagesState?.languages && (
+        <RNPickerSelect
+          onValueChange={handleChangeLanguage}
+          items={_languages || []}
+          value={currentLanguage}
+          style={pickerStyle}
+          useNativeAndroidPickerStyle={false}
+          placeholder={{}}
+          Icon={() => <MaterialIcons name='keyboard-arrow-down' style={pickerStyle.icon} />}
+          disabled={orderState.loading}
+        />
+      )}
+    </Container>
+  )
+}
+
+const pickerStyle = StyleSheet.create({
+  inputAndroid: {
+    color: colors.secundaryContrast,
+    borderWidth: 1,
+    borderColor: 'transparent',
+    borderRadius: 15,
+    paddingHorizontal: 10,
+    backgroundColor: colors.inputDisabled,
+    width: 60,
+  },
+  inputIOS: {
+    color: colors.secundaryContrast,
+    paddingEnd: 20,
+    height: 40,
+    borderWidth: 1,
+    borderColor: 'transparent',
+    borderRadius: 15,
+    paddingHorizontal: 10,
+    backgroundColor: colors.inputDisabled
+  },
+  icon: {
+    top: Platform.OS === 'ios' ? 10 : 15,
+    right: Platform.OS === 'ios' ? 0 : 7,
+    position: 'absolute',
+    fontSize: 20
+  },
+  placeholder: {
+    color: colors.secundaryContrast
+  }
+})
+
+export const LanguageSelector = (props: LanguageSelectorParams) => {
+  const LanguageProps = {
+    ...props,
+    UIComponent: LanguageSelectorUI
+  }
+
+  return <LanguageSelectorController {...LanguageProps} />
+}
