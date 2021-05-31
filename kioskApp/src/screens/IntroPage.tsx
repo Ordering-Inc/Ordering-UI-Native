14d0ea6b7ef13de465d4a9e6938583b1ed0919c8
Bryan Bencomo <bjbh415@gmail.com>
2021-05-31 09:02:33 -0400

    feat: :art: aligns items in IntroPage

--- kioskApp/src/screens/IntroPage.tsx
+++ kioskApp/src/screens/IntroPage.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Dimensions } from 'react-native';
+import { Dimensions, View } from 'react-native';
 
 import { Container } from '../layouts/Container';
 import OImage from '../components/shared/OImage';
@@ -8,28 +8,38 @@ import OButton from '../components/shared/OButton';
 const IntroPage = () => {
   return (
 		<Container>
+			<View
+				style={{
+					height: _dim.height - _offset,
+					justifyContent: 'space-around',
+					alignItems: 'center'
+				}}
+			>
+				<OImage
+					source={require('../assets/images/logo.png')}
+					width={_dim.width - _offset}
+					height={_dim.height * 0.1}
+				/>
+				
+				<OImage
+					source={require('../assets/images/burger.png')}
+					width={_dim.width}
+					height={_dim.height * 0.6}
+				/>
 
-			<OImage
-				source={require('../assets/images/logo.png')}
-				width={_dim.width}
-				height={_dim.height * 0.1}
-      />
-			
-			<OImage
-				source={require('../assets/images/burger.png')}
-				width={_dim.width}
-				height={_dim.height * 0.6}
-      />
-
-			<OButton
-				text="Touch to order"
-				style={{ width: _dim.width * 0.5 }}
-			/>
-			
+				<OButton
+					text="Touch to order"
+					parentStyle={{
+						alignItems: 'center',
+						width: _dim.width - _offset
+					}}
+				/>
+			</View>
 		</Container>
 	);
 };
 
 const _dim = Dimensions.get('window');
+const _offset = 50;
 
 export default IntroPage;
