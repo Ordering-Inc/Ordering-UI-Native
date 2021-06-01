826647c962120307f489d5e163e0b31a5ee0570d
Carlos Quiroz <77166972+carlosq-ordering@users.noreply.github.com>
2021-06-01 16:51:03 -0700

    Fix typo

--- example/ios/delivery/AppDelegate.m
+++ example/ios/delivery/AppDelegate.m
@@ -44,7 +44,7 @@ static void InitializeFlipper(UIApplication *application) {
   rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
 
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
-  UIViewContrapioller *rootViewController = [UIViewController new];
+  UIViewController *rootViewController = [UIViewController new];
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
