41b6d7403528e837fbfe56e967c65e31eae4b91f
Bryan Bencomo <bjbh415@gmail.com>
2021-06-03 21:25:35 -0400

    refactor: :art: create _renderCategories and _renderPromos

--- kioskApp/src/components/BusinessProductsListing/index.tsx
+++ kioskApp/src/components/BusinessProductsListing/index.tsx
@@ -24,49 +24,70 @@ const BusinessProductsListingUI = (props: BusinessProductsListingParams) => {
     errorQuantityProducts,
     header,
     logo
-  } = props
+  } = props;
 
-  const [, t] = useLanguage()
+  const [, t] = useLanguage();
+
+  const _renderTitle = (title: string): React.ReactElement => (
+    <View style={{ paddingHorizontal: 20, paddingVertical: 8 }}>
+      <OText
+        size={_dim.width * 0.09}
+        weight="bold"
+      >
+        {title}
+      </OText>
+    </View>
+  );
+
+  const _renderPromos = (): React.ReactElement => (
+    <>
+      {_renderTitle(t('PROMOS', 'Promos'))}
+    </>
+  );
+
+  const _renderCategories = (): React.ReactElement => (
+    <>    
+      {_renderTitle(t('CATEGORIES', 'Categories'))}
+        
+      <GridContainer
+        style={{ justifyContent: 'space-between' }}
+      >
+        {
+          businessState?.business?.original?.categories.map((category: any) => (
+            <OCard
+              title={category?.name || ''}
+              image={{ uri: category?.image}}
+              onPress={() => {
+                navigation.navigate('Category', {
+                  category,
+                  categories: businessState.business.original.categories,
+                });
+              }}
+              titleStyle={{ textAlign: 'center' }}
+            />
+          ))
+        }
+      </GridContainer>
+    </>
+  );
+
+  if (businessState?.loading) {
+    return <OText>cargando...</OText>
+  }
+
+  if (businessState?.error) {
+    return <OText>error!</OText>
+  }
 
   return (
     <>
-      <View style={{ paddingHorizontal: 20, paddingVertical: 8 }}>
-        <OText
-          size={_dim.width * 0.09}
-          weight="bold"
-        >
-          {t('CATEGORIES', 'Categories')}
-        </OText>
-      </View>
-
-      {businessState?.loading
-        ? <OText>loading categories...</OText>
-        : (
-          <GridContainer
-            style={{
-              justifyContent: 'space-between',
-            }}
-          >
-            {
-              businessState?.business?.original?.categories.map((category: any) => (
-                <OCard
-                  title={category?.name || ''}
-                  image={{ uri: category?.image}}
-                  onPress={() => {
-                    navigation.navigate('Category', {
-                      category,
-                      categories: businessState.business.original.categories,
-                    });
-                  }}
-                  titleStyle={{ textAlign: 'center' }}
-                />
-              ))
-            }
-          </GridContainer>
-        )
-      }
+      {businessState?.business?.original?.promos?.length > 0
+        && _renderPromos()}
+      
+      {businessState?.business?.original?.categories?.length > 0
+        && _renderCategories()}
     </>
-  )
+  );
 }
 
 const _dim = Dimensions.get('window');
