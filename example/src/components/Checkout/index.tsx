e66875a3c56bbcc1a1d98c31ab2a9d6ecade6b6e
carlosq-ordering <carlos.q@ordering.co>
2021-06-24 13:04:02 -0400

    feat: validation

--- example/src/components/Checkout/index.tsx
+++ example/src/components/Checkout/index.tsx
@@ -41,6 +41,13 @@ import {
   ChBusinessDetails,
   ChUserDetails
 } from './styles';
+
+import {
+  OSBill,
+  OSTable,
+  OSTotal,
+  OSCoupon
+} from '../OrderSummary/styles'
 import { Fade, Placeholder, PlaceholderLine } from 'rn-placeholder';
 
 import { ToastType, useToast } from '../../providers/ToastProvider';
@@ -168,6 +175,8 @@ const CheckoutUI = (props: any) => {
       navigation?.canGoBack() && navigation.goBack();
     }
   }, [cart?.products])
+
+  const isCouponEnabled = validationFields?.fields?.checkout?.coupon?.enabled;
 
   return (
     <>
@@ -448,6 +457,91 @@ const CheckoutUI = (props: any) => {
                   isCartPending={cart?.status === 2}
                   isFromCheckout
                 />
+                {cart?.valid && (
+                  <OSBill>
+                    <OSTable>
+                      <OText>{t('SUBTOTAL', 'Subtotal')}</OText>
+                      <OText>
+                        {cart.business.tax_type === 1
+                          ? parsePrice((cart?.subtotal + cart?.tax) || 0)
+                          : parsePrice(cart?.subtotal || 0)}
+                      </OText>
+                    </OSTable>
+                    {cart?.discount > 0 && cart?.total >= 0 && (
+                      <OSTable>
+                        {cart?.discount_type === 1 ? (
+                          <OText>
+                            {t('DISCOUNT', 'Discount')}
+                            <OText>{`(${verifyDecimals(cart?.discount_rate, parsePrice)}%)`}</OText>
+                          </OText>
+                        ) : (
+                          <OText>{t('DISCOUNT', 'Discount')}</OText>
+                        )}
+                        <OText>- {parsePrice(cart?.discount || 0)}</OText>
+                      </OSTable>
+                    )}
+                    {cart.business.tax_type !== 1 && (
+                      <OSTable>
+                        <OText>
+                          {t('TAX', 'Tax')}
+                          {`(${verifyDecimals(cart?.business?.tax, parseNumber)}%)`}
+                        </OText>
+                        <OText>{parsePrice(cart?.tax || 0)}</OText>
+                      </OSTable>
+                    )}
+                    {orderState?.options?.type === 1 && cart?.delivery_price > 0 && (
+                      <OSTable>
+                        <OText>{t('DELIVERY_FEE', 'Delivery Fee')}</OText>
+                        <OText>{parsePrice(cart?.delivery_price)}</OText>
+                      </OSTable>
+                    )}
+                    {cart?.driver_tip > 0 && (
+                      <OSTable>
+                        <OText>
+                          {t('DRIVER_TIP', 'Driver tip')}
+                          {cart?.driver_tip_rate > 0 &&
+                            parseInt(configs?.driver_tip_type?.value, 10) === 2 &&
+                            !parseInt(configs?.driver_tip_use_custom?.value, 10) &&
+                          (
+                            `(${verifyDecimals(cart?.driver_tip_rate, parseNumber)}%)`
+                          )}
+                        </OText>
+                        <OText>{parsePrice(cart?.driver_tip)}</OText>
+                      </OSTable>
+                    )}
+                    {cart?.service_fee > 0 && (
+                      <OSTable>
+                        <OText>
+                          {t('SERVICE_FEE', 'Service Fee')}
+                          {`(${verifyDecimals(cart?.business?.service_fee, parseNumber)}%)`}
+                        </OText>
+                        <OText>{parsePrice(cart?.service_fee)}</OText>
+                      </OSTable>
+                    )}
+                    {isCouponEnabled && !(cart?.status === 2) && (
+                      <OSTable>
+                        <OSCoupon>
+                          <CouponControl
+                            businessId={cart.business_id}
+                            price={cart.total}
+                          />
+                        </OSCoupon>
+                      </OSTable>
+                    )}
+                    {cart?.total >= 1 && (
+                      <OSTotal>
+                        <OSTable style={{ marginTop: 15 }}>
+                          <OText style={{ fontWeight: 'bold' }}>
+                            {t('TOTAL', 'Total')}
+                          </OText>
+                          <OText style={{ fontWeight: 'bold' }} color={colors.primary}>
+                            {parsePrice(cart?.total)}
+                          </OText>
+                        </OSTable>
+                      </OSTotal>
+                    )}
+                  </OSBill>
+                )}
               </>
             )}
           </ChCart>
