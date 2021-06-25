9372d6ede9eda16924c64d25691ace6833d4ec28
carlosq-ordering <carlos.q@ordering.co>
2021-06-25 15:21:36 -0400

    feat: click on login btn

--- example/src/components/LoginForm/styles.tsx
+++ example/src/components/LoginForm/styles.tsx
@@ -1,7 +1,7 @@
 import styled, { css } from 'styled-components/native';
 import { colors } from '../../theme.json'
 
-export const Container = styled.ScrollView`
+export const Container = styled.View`
   padding-bottom: 40px;
 `
 
