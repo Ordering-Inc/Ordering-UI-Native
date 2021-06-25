4e12fe00d306b94af3b0780ac02e763455290615
carlosq-ordering <carlos.q@ordering.co>
2021-06-25 17:08:40 -0400

    feat: fix bug for shared menus

--- example/src/components/Cart/index.tsx
+++ example/src/components/Cart/index.tsx
@@ -227,7 +227,7 @@ const CartUI = (props: any) => {
           isCartProduct
           productCart={curProduct}
           businessSlug={cart?.business?.slug}
-          businessId={curProduct?.business_id}
+          businessId={cart?.business_id}
           categoryId={curProduct?.category_id}
           productId={curProduct?.id}
           onSave={handlerProductAction}
