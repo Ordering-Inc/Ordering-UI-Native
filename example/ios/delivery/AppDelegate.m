6c0af46a58b861cfd969c86fdd1d7d6e755baceb
Carlos Quiroz <77166972+carlosq-ordering@users.noreply.github.com>
2021-06-30 11:39:39 -0700

    [FIX] pin map on ios enviroment

--- example/ios/delivery/AppDelegate.m
+++ example/ios/delivery/AppDelegate.m
@@ -36,6 +36,7 @@ static void InitializeFlipper(UIApplication *application) {
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [GMSServices provideAPIKey:@"AIzaSyCcHUgx-vm4-vUV0i-dCcrdpmKgYZ1EmEs"];
 #ifdef FB_SONARKIT_ENABLED
   InitializeFlipper(application);
 #endif
