e620b0a47835e3e21f3db473a8ed04d4b8aa890a
dreamsky2020 <59742254+dreamsky2020@users.noreply.github.com>
2021-07-23 17:28:17 +0800

    Feat theme uber eats

---
+++ example/src/themes/uberEats/src/components/FacebookLogin/styles.tsx
@@ -0,0 +1,22 @@
+import styled from 'styled-components/native'
+import { colors } from '../../../../../theme.json'
+
+export const Container = styled.View``
+
+export const FacebookButton = styled.TouchableOpacity`
+  background-color: ${colors.white};
+  border-color: ${colors.primary};
+  border-width: 1px;
+  font-size: 16px;
+  padding: 15px 30px;
+  font-size: 16px;
+  font-weight: 400;
+  text-align: center;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+  height: 52px;
+`
