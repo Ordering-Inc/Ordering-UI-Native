fe86efb4ffafe624134f3e4422010d48d0073bac
Jesus Perez <jesusdpch00@gmail.com>
2021-08-06 23:48:58 -0400

    [ADD] out of stock for products with promo

--- example/themes/kiosk/src/components/BusinessProductsListing/index.tsx
+++ example/themes/kiosk/src/components/BusinessProductsListing/index.tsx
@@ -14,10 +14,7 @@ import Spinner from 'react-native-loading-spinner-overlay';
 import { LANDSCAPE, useDeviceOrientation } from '../../../../../src/hooks/DeviceOrientation';
 
 const BusinessProductsListingUI = (props: BusinessProductsListingParams) => {
-  const {
-    navigation,
-    businessState,
-  } = props;
+  const {navigation, businessState} = props;
 
   const business: Business = businessState.business;
 
@@ -28,33 +25,30 @@ const BusinessProductsListingUI = (props: BusinessProductsListingParams) => {
   let _promos: any = [];
 
   _categories?.forEach((categ: any) => {
-    const _featuredProds = categ?.products?.filter((prod: any) => prod.featured);
+    const _featuredProds = categ?.products?.filter(
+      (prod: any) => prod.featured,
+    );
 
     if (_featuredProds?.length > 0) {
-      _promos = [
-        ..._promos,
-        ..._featuredProds,
-      ]
+      _promos = [..._promos, ..._featuredProds];
     }
   });
 
   const _renderTitle = (title: string): React.ReactElement => (
-    <View style={{ paddingHorizontal: 20, paddingVertical: 40 }}>
-      <OText
-        size={orientationState?.dimensions?.width * 0.048}
-        weight="bold"
-      >
+    <View style={{paddingHorizontal: 20, paddingVertical: 40}}>
+      <OText size={orientationState?.dimensions?.width * 0.048} weight="bold">
         {title}
       </OText>
     </View>
   );
 
-  const _renderItem = ({ item, index }: { item: Product, index: number }) => {
+  const _renderItem = ({item, index}: {item: Product; index: number}) => {
     return (
       <PromoCard
         title={item?.name}
-        {...(!!item?.description && { description: item?.description })}
-        image={{ uri: item?.images }}
+        {...(!!item?.description && {description: item?.description})}
+        image={{uri: item?.images}}
+        isOutOfStock={!item?.inventoried}
         onPress={() => {
           navigation.navigate('ProductDetails', {
             businessId: business?.api?.businessId,
@@ -64,7 +58,7 @@ const BusinessProductsListingUI = (props: BusinessProductsListingParams) => {
         }}
       />
     );
-  }
+  };
 
   let _carousel: Carousel<Product> | null;
 
@@ -73,7 +67,9 @@ const BusinessProductsListingUI = (props: BusinessProductsListingParams) => {
       {_renderTitle(t('PROMOS', 'Promos'))}
       <Carousel
         keyExtractor={(item: any) => item.id}
-        ref={(c: any) => { _carousel = c; }}
+        ref={(c: any) => {
+          _carousel = c;
+        }}
         data={_promos}
         renderItem={_renderItem}
         sliderWidth={orientationState?.dimensions?.width}
@@ -88,9 +84,8 @@ const BusinessProductsListingUI = (props: BusinessProductsListingParams) => {
         activeSlideAlignment="start"
         inactiveSlideOpacity={1}
         initialScrollIndex={_carousel?.currentIndex}
-        onScrollToIndexFailed={(_: any) => { }}
+        onScrollToIndexFailed={(_: any) => {}}
       />
-
     </>
   );
 
@@ -98,55 +93,50 @@ const BusinessProductsListingUI = (props: BusinessProductsListingParams) => {
     <>
       {_renderTitle(t('CATEGORIES', 'Categories'))}
       <GridContainer>
-        {
-          _categories.map((category: any) => (
-            <OCard
-              key={category.id}
-              title={category?.name || ''}
-              image={{ uri: category?.image }}
-              style={{
-                width: orientationState?.orientation === LANDSCAPE
+        {_categories.map((category: any) => (
+          <OCard
+            key={category.id}
+            title={category?.name || ''}
+            image={{uri: category?.image}}
+            style={{
+              width:
+                orientationState?.orientation === LANDSCAPE
                   ? orientationState?.dimensions?.width * 0.16
                   : orientationState?.dimensions?.width * 0.21,
-              }}
-              onPress={() => {
-                navigation.navigate('Category', {
-                  category,
-                  categories: business.original.categories,
-                  businessId: business?.api?.businessId,
-                  businessSlug: business?.slug,
-                });
-              }}
-              titleStyle={{ textAlign: 'center' }}
-            />
-          ))
-        }
+            }}
+            onPress={() => {
+              navigation.navigate('Category', {
+                category,
+                categories: business.original.categories,
+                businessId: business?.api?.businessId,
+                businessSlug: business?.slug,
+              });
+            }}
+            titleStyle={{textAlign: 'center'}}
+          />
+        ))}
       </GridContainer>
     </>
   );
 
   if (businessState?.error) {
-    return <OText>error!</OText>
+    return <OText>error!</OText>;
   }
 
   return (
     <>
       <Spinner visible={businessState?.loading} />
 
-      {_promos?.length > 0
-        && _renderPromos()}
-      {_categories?.length > 0
-        && _renderCategories()}
+      {_promos?.length > 0 && _renderPromos()}
+      {_categories?.length > 0 && _renderCategories()}
     </>
   );
-}
+};
 
 export const BusinessProductsListing = (props: any) => {
   const businessProductslistingProps = {
     ...props,
-    UIComponent: BusinessProductsListingUI
-  }
-  return (
-    <BusinessAndProductList {...businessProductslistingProps} />
-  )
-}
+    UIComponent: BusinessProductsListingUI,
+  };
+  return <BusinessAndProductList {...businessProductslistingProps} />;
+};
