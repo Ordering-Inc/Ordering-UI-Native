ea29f2d449c1b4a3eb75b13381a9d88ca1176c9a
Yorman Rodriguez <yorman2414@gmail.com>
2021-08-15 20:06:16 -0400

    Feat: added unread messages in the messages view

--- example/themes/business/src/components/PreviousMessages/styles.tsx
+++ example/themes/business/src/components/PreviousMessages/styles.tsx
@@ -22,3 +22,16 @@ export const Information = styled.View`
   flex: 1;
   min-height: 64px;
 `;
+
+export const Header = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+`;
+
+export const Badge = styled.View`
+  background-color: #e9f2fe;
+  border-radius: 4px;
+  padding: 1px 4px;
+`;
