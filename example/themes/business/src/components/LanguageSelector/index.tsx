c6916e2e019f9690d2e0262a74e95ede0420d708
Breyner Sanchez <breinersanchez97@hotmail.com>
2021-08-18 22:38:04 -0400

    add translate and others countrys

--- example/themes/business/src/components/LanguageSelector/index.tsx
+++ example/themes/business/src/components/LanguageSelector/index.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { TouchableOpacity, View } from 'react-native';
-import { LanguageSelector as LanguageSelectorController } from 'ordering-components/native';
+import { LanguageSelector as LanguageSelectorController, useLanguage } from 'ordering-components/native';
 import CountryPicker, { Flag } from 'react-native-country-picker-modal';
 
 import { Container, LanguageItem } from './styles';
@@ -11,92 +11,94 @@ import { useTheme } from 'styled-components/native';
 
 const LanguageSelectorUI = (props: LanguageSelectorParams) => {
   const { languagesState, currentLanguage, handleChangeLanguage } = props;
+  const [, t] = useLanguage();
 
   const _languages = languagesState?.languages?.map((language: any) => {
-    return {
-      key: language?.code,
-      value: language?.code,
-      label: language?.name,
-      inputLabel: language?.code.toUpperCase(),
-      countryCode: langCountries.find(item => item.value == language?.code)
-        ?.countryCode,
-    };
+	return {
+	  key: language?.code,
+	  value: language?.code,
+	  label: language?.name,
+	  inputLabel: language?.code.toUpperCase(),
+	  countryCode: langCountries.find(item => item.value == language?.code)
+		?.countryCode,
+	};
   });
   _languages &&
-    _languages?.sort((a: any, b: any) =>
-      a.content > b.content ? 1 : b.content > a.content ? -1 : 0,
-    );
+	_languages?.sort((a: any, b: any) =>
+	  a.content > b.content ? 1 : b.content > a.content ? -1 : 0,
+	);
 
   const [isCountryModalVisible, setCountryModalVisible] = useState(false);
 
   const countryCodes = _languages?.map((item: any) => item.countryCode);
 
   const currentLanguageData = _languages?.find(
-    (item: any) => item.value == currentLanguage,
+	(item: any) => item.value == currentLanguage,
   );
 
   const theme = useTheme();
 
   return (
-    <Container style={{ backgroundColor: theme.colors.inputChat }}>
-      {languagesState?.languages && (
-        <CountryPicker
-          countryCode={currentLanguageData}
-          visible={isCountryModalVisible}
-          onClose={() => setCountryModalVisible(false)}
-          withCountryNameButton
-          countryCodes={countryCodes}
-          renderFlagButton={() => (
-            <TouchableOpacity onPress={() => setCountryModalVisible(true)}>
-              <LanguageItem>
-                <Flag
-                  withEmoji
-                  flagSize={24}
-                  countryCode={currentLanguageData?.countryCode}
-                />
-                <OText>{currentLanguageData?.label}</OText>
-              </LanguageItem>
-            </TouchableOpacity>
-          )}
-          flatListProps={{
-            /* @ts-ignore */
-            keyExtractor: item => item.value,
-            data: _languages || [],
-            renderItem: ({ item }) => (
-              <TouchableOpacity
-                onPress={() => {
-                  /* @ts-ignore */
-                  handleChangeLanguage(item.value);
-                  setCountryModalVisible(false);
-                }}>
-                <LanguageItem>
-                  <View style={{ width: 40 }} />
-                  <Flag
-                    withEmoji
-                    flagSize={24}
-                    /* @ts-ignore */
-                    countryCode={item.countryCode}
-                  />
-                  <OText>
-                    {
-                      /* @ts-ignore */
-                      item.label
-                    }
-                  </OText>
-                </LanguageItem>
-              </TouchableOpacity>
-            ),
-          }}
-        />
-      )}
-    </Container>
+	<Container style={{ backgroundColor: theme.colors.inputChat }}>
+	  {languagesState?.languages && (
+		<CountryPicker
+		  countryCode={currentLanguageData}
+		  visible={isCountryModalVisible}
+		  onClose={() => setCountryModalVisible(false)}
+		  withCountryNameButton
+		  countryCodes={countryCodes}
+		  renderFlagButton={() => (
+			<TouchableOpacity onPress={() => setCountryModalVisible(true)}>
+			  <LanguageItem>
+				<Flag
+				  withEmoji
+				  flagSize={24}
+				  countryCode={currentLanguageData?.countryCode}
+				/>
+				<OText>{currentLanguageData?.label}</OText>
+			  </LanguageItem>
+			</TouchableOpacity>
+		  )}
+		  flatListProps={{
+			/* @ts-ignore */
+			keyExtractor: item => item.value,
+			data: _languages || [],
+			renderItem: ({ item }) => (
+			  <TouchableOpacity
+				onPress={() => {
+				  /* @ts-ignore */
+				  handleChangeLanguage(item.value);
+				  setCountryModalVisible(false);
+				}}>
+				<LanguageItem>
+				  <View style={{ width: 40 }} />
+				  <Flag
+					withEmoji
+					flagSize={24}
+					/* @ts-ignore */
+					countryCode={item.countryCode}
+				  />
+				  <OText>
+					{
+					  /* @ts-ignore */
+					  t(item.code, item.label)
+					
+					}
+				  </OText>
+				</LanguageItem>
+			  </TouchableOpacity>
+			),
+		  }}
+		/>
+	  )}
+	</Container>
   );
 };
 
 export const LanguageSelector = (props: LanguageSelectorParams) => {
   const LanguageProps = {
-    ...props,
-    UIComponent: LanguageSelectorUI,
+	...props,
+	UIComponent: LanguageSelectorUI,
   };
 
   return <LanguageSelectorController {...LanguageProps} />;
