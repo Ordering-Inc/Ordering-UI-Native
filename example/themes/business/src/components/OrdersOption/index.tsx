fea33df0dc2170acce03b5637755b0d778911acf
Jesus Perez <jesusdpch00@gmail.com>
2021-09-20 21:18:55 -0400

    [add] fetch only one time when changes status filt

--- example/themes/business/src/components/OrdersOption/index.tsx
+++ example/themes/business/src/components/OrdersOption/index.tsx
@@ -109,17 +109,24 @@ const OrdersOptionUI = (props: OrdersOptionParams) => {
   ];
 
   const tabs = [
-    { key: 0, text: t('PENDING', 'Pending'), tags: [0, 13] },
+    { key: 0, text: t('PENDING', 'Pending'), tags: [0, 13], title: 'pending' },
     {
       key: 1,
       text: t('IN_PROGRESS', 'In Progress'),
       tags: [3, 4, 7, 8, 9, 14, 18, 19, 20, 21],
+      title: 'inProgress',
     },
-    { key: 2, text: t('COMPLETED', 'Completed'), tags: [1, 11, 15] },
+    {
+      key: 2,
+      text: t('COMPLETED', 'Completed'),
+      tags: [1, 11, 15],
+      title: 'completed',
+    },
     {
       key: 3,
       text: t('CANCELLED', 'Cancelled'),
       tags: [2, 5, 6, 10, 12, 16, 17],
+      title: 'cancelled',
     },
   ];
 
@@ -130,6 +137,13 @@ const OrdersOptionUI = (props: OrdersOptionParams) => {
   const [tabsFilter, setTabsFilter] = useState(tabs[0].tags);
   const [tagsFilter, setTagsFilter] = useState(tabs[0].tags);
   const [loadingTag, setLoadingTag] = useState(false);
+  const [isLoadedOrders, setIsLoadedOrders] = useState<any>({
+    pending: { isFetched: true, hasMorePagination: false },
+    inProgress: { isFetched: false, hasMorePagination: false },
+    completed: { isFetched: false, hasMorePagination: false },
+    cancelled: { isFetched: false, hasMorePagination: false },
+  });
+  const [currentTab, setCurrentTab] = useState(tabs[0].title);
   const [reload, setReload] = useState(false);
   const [orientation, setOrientation] = useState(
     Dimensions.get('window').width < Dimensions.get('window').height
@@ -140,11 +154,17 @@ const OrdersOptionUI = (props: OrdersOptionParams) => {
     parseInt(parseFloat(String(Dimensions.get('window').width)).toFixed(0)),
   );
 
-  const handleChangeTab = (tags: number[]) => {
+  const handleChangeTab = (tags: number[], tabTitle: string) => {
     setTabsFilter(tags);
     setRememberOrderStatus(tags);
     setUpdateOtherStatus(tags);
-    loadOrders && loadOrders(true, tags);
+    if (!isLoadedOrders[tabTitle].isFetched) {
+      loadOrders && loadOrders(true, tags);
+      setIsLoadedOrders({
+        ...isLoadedOrders,
+        [tabTitle]: { isFetched: true, hasMorePagination: false },
+      });
+    }
     setTagsFilter(tags);
 
     const ordersTab = values.filter((order: any) =>
@@ -165,6 +185,7 @@ const OrdersOptionUI = (props: OrdersOptionParams) => {
     });
 
     setOrdersToShow(ordersTab);
+    setCurrentTab(tabTitle);
   };
 
   const handleChangeTag = (key: number) => {
@@ -175,20 +196,18 @@ const OrdersOptionUI = (props: OrdersOptionParams) => {
     } else {
       updateTags.push(...tagsFilter.concat(key));
     }
-
-    setOrdersToShow([]);
+    const arr = ordersToShow.filter(
+      (order : any) =>
+        tabsFilter.includes(order.status) && tagsFilter.includes(order.status),
+    );
     setRememberOrderStatus(updateTags);
     setTagsFilter(updateTags);
     setUpdateOtherStatus(updateTags);
-
-    if (updateTags.length) {
-      setLoadingTag(true);
-      loadOrders && loadOrders(true, updateTags);
-    }
   };
 
   const handleReload = () => {
     setReload(true);
+    setOrdersToShow([]);
     loadOrders &&
       loadOrders(
         true,
@@ -213,7 +232,6 @@ const OrdersOptionUI = (props: OrdersOptionParams) => {
     let ordersTab = values?.filter((order: any) =>
       tagsFilter.some(tag => tag === order.status),
     );
-
     let hash: any = {};
     ordersTab = ordersTab.filter((order: any) =>
       hash[order.id] ? false : (hash[order.id] = true),
@@ -227,6 +245,24 @@ const OrdersOptionUI = (props: OrdersOptionParams) => {
     if (loadingTag) setLoadingTag(false);
   }, [loading]);
 
+  useEffect(() => {
+    setOrdersToShow(
+      values.filter((order: any) => tagsFilter.includes(order.status)),
+    );
+  }, [tagsFilter]);
+
+  useEffect(() => {
+    if (!loading) {
+      setIsLoadedOrders({
+        ...isLoadedOrders,
+        [currentTab]: {
+          isFetched: true,
+          hasMorePagination: pagination.currentPage < pagination.totalPages,
+        },
+      });
+    }
+  }, [pagination.totalPages]);
+
   Dimensions.addEventListener('change', ({ window: { width, height } }) => {
     setWindowsWidth(
       parseInt(parseFloat(String(Dimensions.get('window').width)).toFixed(0)),
@@ -238,6 +274,7 @@ const OrdersOptionUI = (props: OrdersOptionParams) => {
       setOrientation('Landscape');
     }
   });
+
 
   const styles = StyleSheet.create({
     header: {
@@ -297,24 +334,6 @@ const OrdersOptionUI = (props: OrdersOptionParams) => {
     <>
       <View style={styles.header}>
         <OText style={styles.title}>{t('MY_ORDERS', 'My orders')}</OText>
-
-        <View style={styles.icons}>
-          <OIconButton
-            icon={theme.images.general.reload}
-            borderColor={theme.colors.clear}
-            iconStyle={{ width: 25, height: 25 }}
-            style={{ maxWidth: 40, height: 35 }}
-            onClick={handleReload}
-          />
-
-          {/* <OIconButton
-            icon={theme.images.general.search}
-            borderColor={theme.colors.clear}
-            iconStyle={{ width: 25, height: 25 }}
-            style={{ maxWidth: 40, height: 35 }}
-            onClick={() => {}}
-          /> */}
-        </View>
       </View>
 
       <FiltersTab>
@@ -328,7 +347,7 @@ const OrdersOptionUI = (props: OrdersOptionParams) => {
               <Pressable
                 key={tab.key}
                 style={styles.pressable}
-                onPress={() => handleChangeTab(tab.tags)}>
+                onPress={() => handleChangeTab(tab.tags, tab.title)}>
                 <OText
                   style={styles.tab}
                   color={
@@ -352,7 +371,8 @@ const OrdersOptionUI = (props: OrdersOptionParams) => {
                         ? theme.colors.textGray
                         : theme.colors.tabBar,
                     borderBottomWidth: 2,
-                  }}></View>
+                  }}
+                />
               </Pressable>
             ))}
           </TabsContainer>
@@ -403,6 +423,7 @@ const OrdersOptionUI = (props: OrdersOptionParams) => {
             orders={ordersToShow}
             onNavigationRedirect={onNavigationRedirect}
             getOrderStatus={getOrderStatus}
+            tabsFilter={tabsFilter}
           />
         )}
 
@@ -442,7 +463,8 @@ const OrdersOptionUI = (props: OrdersOptionParams) => {
           pagination.totalPages &&
           !loading &&
           !!orders.length &&
-          pagination.currentPage < pagination.totalPages && (
+          pagination.currentPage < pagination.totalPages &&
+          isLoadedOrders[currentTab].hasMorePagination && (
             <OButton
               onClick={handleLoadMore}
               text={t('LOAD_MORE_ORDERS', 'Load more orders')}
