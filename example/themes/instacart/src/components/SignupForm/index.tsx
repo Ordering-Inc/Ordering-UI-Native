7940e6cda33fc568067b44caa6f946c669a78d92
dreamsky2020 <59742254+dreamsky2020@users.noreply.github.com>
2021-09-29 22:05:55 +0300

    [feat] add the terms and conditions

--- example/themes/instacart/src/components/SignupForm/index.tsx
+++ example/themes/instacart/src/components/SignupForm/index.tsx
@@ -1,5 +1,5 @@
 import React, { createRef, useEffect, useRef, useState } from 'react';
-import { View, Pressable, StyleSheet, Keyboard } from 'react-native';
+import { View, Pressable, StyleSheet, Keyboard, Linking } from 'react-native';
 import { useForm, Controller } from 'react-hook-form';
 import Spinner from 'react-native-loading-spinner-overlay';
 
@@ -31,6 +31,7 @@ import NavBar from '../NavBar'
 import { VerifyPhone } from '../VerifyPhone';
 
 import { OText, OButton, OInput, OModal } from '../shared';
+import CheckBox from '@react-native-community/checkbox';
 import { SignupParams } from '../../types';
 import { sortInputFields } from '../../utils';
 import { useTheme } from 'styled-components/native';
@@ -249,6 +250,16 @@ const SignupFormUI = (props: SignupParams) => {
 		onChange(value.toLowerCase().replace(/[&,()%";:ç?<>{}\\[\]\s]/g, ''))
 	}
 
+	const handleOpenTermsUrl = async (url: any) => {
+		const supported = await Linking.canOpenURL(url);
+	
+		if (supported) {
+		  	await Linking.openURL(url);
+		} else {
+		  	showToast(ToastType.Error, t('VALIDATION_ERROR_ACTIVE_URL', 'The _attribute_ is not a valid URL.').replace('_attribute_', t('URL', 'URL')))
+		}
+	}
+
 	useEffect(() => {
 		if (!formState.loading && formState.result?.error) {
 			formState.result?.result && showToast(
@@ -436,6 +447,43 @@ const SignupFormUI = (props: SignupParams) => {
 						</>
 					) : (
 						<Spinner visible />
+					)}
+
+					{configs?.terms_and_conditions?.value === 'true' && (
+						<View style={{ flexDirection: 'row', alignItems: 'center', marginBottom: 20 }}>
+							<Controller
+								control={control}
+								render={({ onChange, value }: any) => (
+								<CheckBox
+									value={value}
+									onValueChange={newValue => {
+										onChange(newValue)
+									}}
+									tintColors={{
+										true: theme.colors.primary,
+										false: theme.colors.disabled
+									}}
+									tintColor={theme.colors.disabled}
+									onCheckColor={theme.colors.primary}
+								/>
+								)}
+								name='termsAccept'
+								rules={{
+									required: t('VALIDATION_ERROR_ACCEPTED', 'The _attribute_ must be accepted.').replace('_attribute_', t('TERMS_AND_CONDITIONS', 'Terms & Conditions'))
+								}}
+								defaultValue={false}
+							/>
+							<OText style={{ ...theme.labels.normal }}>{t('TERMS_AND_CONDITIONS_TEXT', 'I’m agree with')}</OText>
+							<OButton
+								imgRightSrc={null}
+								text={t('TERMS_AND_CONDITIONS', 'Terms & Conditions')}
+								bgColor='#FFF'
+								borderColor='#FFF'
+								style={{ paddingLeft: 0, paddingRight: 0, height: 30 }}
+								textStyle={{ color: theme.colors.primary, marginLeft: 5, marginRight: 5 }}
+								onClick={() => handleOpenTermsUrl(configs?.terms_and_conditions_url?.value)}
+							/>
+						</View>
 					)}
 
 					{signupTab === 'cellphone' && useSignupByEmail && useSignupByCellphone ? (
