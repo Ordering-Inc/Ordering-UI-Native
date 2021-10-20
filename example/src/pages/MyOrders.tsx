97026555ca57fb28a23f07f739e1f8ac3675aa7f
dreamsky2020 <59742254+dreamsky2020@users.noreply.github.com>
2021-10-21 01:06:40 +0800

    recover

--- example/src/pages/MyOrders.tsx
+++ example/src/pages/MyOrders.tsx
@@ -1,37 +1,40 @@
-import React from 'react'
-import { StyleSheet } from 'react-native';
-import { SafeAreaContainerLayout } from '../../themes/business/src/layouts/SafeAreaContainer'
-import { OrdersOption } from '../../themes/business/src/components/OrdersOption'
-import { useTheme } from 'styled-components/native'
+import React, {useState} from 'react'
+import { useLanguage } from 'ordering-components/native'
+import { OrdersOption } from '../components/OrdersOption'
+import { OText } from '../components/shared'
+import { Container } from '../layouts/Container'
 
 const MyOrders = ({ navigation }: any) => {
 
-  const theme = useTheme()
-
-  const MyOrderProps = {
+  const [, t] = useLanguage()
+  const [ordersLength,setOrdersLength] = useState({activeOrdersLength: 0, previousOrdersLength: 0})
+  const myOrderProps = {
     navigation,
     onNavigationRedirect: (page: string, params: any) => {
-      if (!page) return;
+      if (!page) return
       navigation.navigate(page, params);
-    },
-    paginationSettings: {
-      initialPage: 1,
-      pageSize: 50,
-      controlType: 'infinity'
-    },
+    }
   }
 
-  const styles = StyleSheet.create({
-    container: {
-      backgroundColor: theme.colors.backgroundLight,
-    },
-  });
 
   return (
-    <SafeAreaContainerLayout style={styles.container}>
-      <OrdersOption {...MyOrderProps} />
-    </SafeAreaContainerLayout>
-  );
-};
+    <Container>
+      <OText size={24} mBottom={20}>
+        {t('MY_ORDERS', 'My Orders')}
+      </OText>
+      <OrdersOption
+        {...myOrderProps}
+        activeOrders
+        ordersLength={ordersLength}
+        setOrdersLength={setOrdersLength}
+      />
+      <OrdersOption
+        {...myOrderProps}
+        ordersLength={ordersLength}
+        setOrdersLength={setOrdersLength}
+      />
+    </Container>
+  )
+}
 
 export default MyOrders;
