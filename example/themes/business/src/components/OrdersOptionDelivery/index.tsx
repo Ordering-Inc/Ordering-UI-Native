6c891050ec8bf45928e4046766f541b6ddd1959f
Min <skyandarti@outlook.com>
2021-10-26 06:36:50 +0800

    Remove: naked.js

--- example/themes/business/src/components/OrdersOptionDelivery/index.tsx
+++ example/themes/business/src/components/OrdersOptionDelivery/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { useLanguage } from 'ordering-components/native';
 import { useTheme } from 'styled-components/native';
 import { Container } from './styles';
