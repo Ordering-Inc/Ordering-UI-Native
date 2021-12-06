98e4d90e22206897ebec2ec73f0ead2f3fef412f
carlosq-ordering <carlos.q@ordering.co>
2021-12-06 11:15:09 -0400

    update franchise template

--- example/themes/franchises/src/components/SignupForm/index.tsx
+++ example/themes/franchises/src/components/SignupForm/index.tsx
@@ -95,7 +95,7 @@ const SignupFormUI = (props: SignupParams) => {
 	const showInputPhoneNumber =
 		validationFields?.fields?.checkout?.cellphone?.enabled ?? false;
 
-	const { showToast } = useToast();
+  const [, { showToast }] = useToast();
 	const [, t] = useLanguage();
 	const [, { login }] = useSession();
 	const [{ configs }] = useConfig();
