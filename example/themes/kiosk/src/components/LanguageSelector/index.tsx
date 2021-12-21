9c1dbba5bba18caeda6f1f561c5ad0c4134ef6e8
Carlos Gomez <carlos.g@ordering.co>
2021-12-21 13:35:16 -0600

    [ADD] skeleton in language selector

--- example/themes/kiosk/src/components/LanguageSelector/index.tsx
+++ example/themes/kiosk/src/components/LanguageSelector/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { TouchableOpacity, View, StyleSheet } from 'react-native'
 import { LanguageSelector as LanguageSelectorController, useLanguage } from 'ordering-components/native'
 import CountryPicker, { Flag } from 'react-native-country-picker-modal'
@@ -9,6 +9,7 @@ import { LanguageSelectorParams } from '../../types'
 import { OText } from '../shared'
 import MatarialIcon from "react-native-vector-icons/MaterialIcons"
 import { useTheme } from 'styled-components/native'
+import { Fade, Placeholder, PlaceholderLine } from 'rn-placeholder'
 const LanguageSelectorUI = (props: LanguageSelectorParams) => {
   const {
     languagesState,
@@ -18,7 +19,7 @@ const LanguageSelectorUI = (props: LanguageSelectorParams) => {
 
 	const theme = useTheme()
   const [{loading}] = useLanguage()
-
+  const [isLoading, setIsLoading] = useState(true)
   const styles = StyleSheet.create({
     closeIcon: {
       width: 48, marginLeft: 32
@@ -27,6 +28,15 @@ const LanguageSelectorUI = (props: LanguageSelectorParams) => {
       marginLeft: 40
     }
   })
+  useEffect(() => {
+    if (!isLoading) return
+    const loadingLanguage = setTimeout(() =>{
+      setIsLoading(false)
+    }, 1000);
+    return () => {
+      clearTimeout(loadingLanguage);
+    }
+  }, [])
 
   const _languages = languagesState?.languages?.map((language: any) => {
     return {
@@ -48,63 +58,73 @@ const LanguageSelectorUI = (props: LanguageSelectorParams) => {
 	const currentLanguageData = _languages?.find((item:any) => item.value == currentLanguage);
 
 	return (
-    <Container>
-      {languagesState?.languages && (
-				<CountryPicker
-					countryCode={currentLanguageData?.countryCode}
-					visible={isCountryModalVisible}
-					onClose={() => setCountryModalVisible(false)}
-					withCountryNameButton
-					countryCodes={countryCodes}
-					closeButtonStyle={styles.closeIcon}
-					renderFlagButton={() => (
-						<TouchableOpacity
-							onPress={() => setCountryModalVisible(true)}
-							disabled={loading}
-						>
-							<LanguageItem>
-									<Flag
-										withEmoji
-										flagSize={24}
-										countryCode={currentLanguageData?.countryCode}
-									/>
-								<OText color={theme.colors.primary}>{currentLanguageData?.label}</OText>
-								<MatarialIcon name='keyboard-arrow-down' size={24}/>
-							</LanguageItem>
-						</TouchableOpacity>
+		<>
+		{ isLoading ? 
+		(<Container>
+			<Placeholder  style={{ width: 130, paddingTop: 10 }} Animation={Fade}>
+				<PlaceholderLine height={15}/>
+			</Placeholder>
+		</Container>
+		):(
+			<Container>
+			{languagesState?.languages && (
+						<CountryPicker
+							countryCode={currentLanguageData?.countryCode}
+							visible={isCountryModalVisible}
+							onClose={() => setCountryModalVisible(false)}
+							withCountryNameButton
+							countryCodes={countryCodes}
+							closeButtonStyle={styles.closeIcon}
+							renderFlagButton={() => (
+								<TouchableOpacity
+									onPress={() => setCountryModalVisible(true)}
+									disabled={loading}
+								>
+									<LanguageItem>
+											<Flag
+												withEmoji
+												flagSize={24}
+												countryCode={currentLanguageData?.countryCode}
+											/>
+										<OText color={theme.colors.primary}>{currentLanguageData?.label}</OText>
+										<MatarialIcon name='keyboard-arrow-down' size={24}/>
+									</LanguageItem>
+								</TouchableOpacity>
+							)}
+							flatListProps={{
+								/* @ts-ignore */
+								keyExtractor: (item) => item.value,
+								data: _languages || [],
+								renderItem: ({item} : any) => (
+									<TouchableOpacity
+										onPress={() => {
+											/* @ts-ignore */
+											handleChangeLanguage(item.value);
+											setCountryModalVisible(false);
+										}}
+										disabled={loading}
+									>
+										<LanguageItem>
+											<View style={styles.flagsContainer} />
+											<Flag
+												withEmoji
+												flagSize={24}
+												/* @ts-ignore */
+												countryCode={item.countryCode}
+											/>
+											<OText>{
+												/* @ts-ignore */
+												item.label
+											}</OText>
+										</LanguageItem>
+									</TouchableOpacity>
+								)
+							}}
+						/>
 					)}
-					flatListProps={{
-						/* @ts-ignore */
-						keyExtractor: (item) => item.value,
-						data: _languages || [],
-						renderItem: ({item} : any) => (
-							<TouchableOpacity
-								onPress={() => {
-									/* @ts-ignore */
-									handleChangeLanguage(item.value);
-									setCountryModalVisible(false);
-								}}
-								disabled={loading}
-							>
-								<LanguageItem>
-									<View style={styles.flagsContainer} />
-									<Flag
-										withEmoji
-										flagSize={24}
-										/* @ts-ignore */
-										countryCode={item.countryCode}
-									/>
-									<OText>{
-										/* @ts-ignore */
-										item.label
-									}</OText>
-								</LanguageItem>
-							</TouchableOpacity>
-						)
-					}}
-				/>
-			)}
-    </Container>
+			</Container>
+		)}
+	</>
   )
 }
 
