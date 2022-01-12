c53ae3e2e1ad1097f6aa7343b780d3aaf5917b0e
carlosq-ordering <carlos.q@ordering.co>
2022-01-12 19:22:28 -0400

    delete franchise theme

--- example/themes/franchises/src/components/OrdersOption/styles.tsx
+++
@@ -1,5 +0,0 @@
-import styled from 'styled-components/native'
-
-export const OptionTitle = styled.View`
-	margin-top: 24px;
-`
--- example/themes/franchises/src/components/TaxInformation/styles.tsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components/native'
-
-export const TaxInformationContainer = styled.ScrollView`
-  padding: 10px;
-`
-
-export const ProductContainer = styled.View`
-  flex: 1
-`
