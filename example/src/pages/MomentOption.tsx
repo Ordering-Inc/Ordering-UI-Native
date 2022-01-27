79585d9ed0f8006ebf9ea39734b87afc33e33c6f
dreamsky2020 <59742254+dreamsky2020@users.noreply.github.com>
2022-01-28 04:34:32 +0800

    revert change

--- example/src/pages/MomentOption.tsx
+++ example/src/pages/MomentOption.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useConfig } from 'ordering-components/native'
-import { MomentOption as MomentOptionController } from '../../themes/original/src/components/MomentOption'
+import { MomentOption as MomentOptionController } from '../components/MomentOption'
 
 const MomentOption = ({ navigation, props }: any) => {
   const [{ configs }] = useConfig()
