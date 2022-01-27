4b17f0518a9527236574653a304bc5d765d5cb12
dreamsky2020 <59742254+dreamsky2020@users.noreply.github.com>
2022-01-28 04:21:21 +0800

    improve design

--- example/themes/original/src/components/MomentOption/index.tsx
+++ example/themes/original/src/components/MomentOption/index.tsx
@@ -32,6 +32,8 @@ import {
 import CalendarPicker from 'react-native-calendar-picker';
 import { TouchableRipple } from 'react-native-paper';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome';
+import SelectDropdown from 'react-native-select-dropdown';
 
 const MomentOptionUI = (props: MomentOptionParams) => {
 	const {
@@ -45,6 +47,8 @@ const MomentOptionUI = (props: MomentOptionParams) => {
 		handleChangeDate,
 		handleChangeTime,
 	} = props;
+
+	const countries = ["Egypt", "Canada", "Australia", "Ireland"]
 
 	const theme = useTheme();
 
@@ -171,7 +175,6 @@ const MomentOptionUI = (props: MomentOptionParams) => {
 			},
 		};
 	};
-
 	return (
 		<>
 		<Container style={{ paddingLeft: 40, paddingRight: 40 }}>
@@ -229,8 +232,56 @@ const MomentOptionUI = (props: MomentOptionParams) => {
 						{datesList.length > 0 && (
 							<View style={styles.dateWrap}>
 								<View style={styles.dateLabel}>
-									<OText size={12} color={theme.colors.disabled}>{dateSelected}</OText>
+									<OText size={12} color={theme.colors.textNormal}>{dateSelected}</OText>
 								</View>
+								<SelectDropdown
+									defaultButtonText={timeSelected ? timeSelected : t('DELIVERY_TIME', 'Delivery Time')}
+									defaultValue={74}
+									data={hoursList}
+									disabled={orderState.loading}
+									onSelect={(selectedItem, index) => {
+										setSelectedTime(selectedItem.startTime)
+									}}
+									buttonTextAfterSelection={(selectedItem, index) => {
+										return `${selectedItem.startTime} - ${selectedItem.endTime}`
+									}}
+									rowTextForSelection={(item, index) => {
+										return `${item.startTime} - ${item.endTime}`
+									}}
+									buttonStyle={{
+										backgroundColor: theme.colors.white,
+										borderColor: theme.colors.border,
+										borderWidth: 1,
+										borderRadius: 8,
+										height: 40,
+										width: '100%',
+										flexDirection: 'column',
+										alignItems: 'flex-start',
+										marginBottom: 20
+									}}
+									buttonTextStyle={{
+										color: theme.colors.textNormal,
+										fontSize: 12,
+										paddingTop: 10
+									}}
+									dropdownStyle={{
+										borderRadius: 8,
+										borderColor: theme.colors.lightGray,
+									}}
+									rowStyle={{
+										borderBottomColor: theme.colors.white,
+										backgroundColor: theme.colors.white,
+										height: 40,
+										flexDirection: 'column',
+										alignItems: 'flex-start',
+										paddingTop: 8,
+										paddingLeft: 22
+									}}
+									rowTextStyle={{
+										color: theme.colors.textNormal,
+										fontSize: 14,
+									}}
+								/>							
 								<CalendarPicker
 									nextTitle=">"
 									width={width - 80}
@@ -263,50 +314,6 @@ const MomentOptionUI = (props: MomentOptionParams) => {
 									minDate={moment()}
 								/>
 							</View>
-						)}
-
-						{hoursList.length > 0 && optionSelected.isSchedule && (
-							<>
-								<TouchableRipple style={styles.timeLabel} onPress={() => { setToggleTime(!toggleTime) }}>
-									<>
-										<OIcon src={theme.images.general.clock} width={16} />
-										<OText style={{ flexGrow: 1, paddingHorizontal: 12 }} color={theme.colors.disabled}>{timeSelected ? timeSelected : t('DELIVERY_TIME', 'Delivery Time')}</OText>
-										<OIcon src={theme.images.general.arrow_down} width={16} style={{ transform: [{ rotate: toggleTime ? '180deg' : '0deg' }] }} />
-									</>
-								</TouchableRipple>
-								{toggleTime ? (
-
-									<WrapHours nestedScrollEnabled={true}>
-										<Hours name="hours">
-											{hoursList.map((hour: any, i: any) => (
-												<Hour
-													key={i}
-													onPress={() => setSelectedTime(hour.startTime)}
-													disabled={orderState.loading}
-													style={{ borderColor: selectedTime === hour.startTime ? theme.colors.primary : theme.colors.border }}
-												>
-													<OText
-														color={
-															selectedTime === hour.startTime
-																? theme.colors.primary
-																: theme.colors.textSecondary
-														}>
-														{configs?.format_time?.value === '12'
-															? hour.startTime.includes('12')
-																? `${hour.startTime}PM`
-																: parseTime(moment(hour.startTime, 'HH:mm'), {
-																	outputFormat: 'hh:mma',
-																})
-															: parseTime(moment(hour.startTime, 'HH:mm'), {
-																outputFormat: 'HH:mm',
-															})}
-													</OText>
-												</Hour>
-											))}
-										</Hours>
-									</WrapHours>
-								) : null}
-							</>
 						)}
 					</WrapDelveryTime>
 				)}
