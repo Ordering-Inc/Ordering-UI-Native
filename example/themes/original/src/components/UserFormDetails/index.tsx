591d8356b22a9d2eed5f6d26a55fcc435cbf5d4c
Min <skyandarti@outlook.com>
2022-02-10 03:36:10 +0800

    Update: code

--- example/themes/original/src/components/UserFormDetails/index.tsx
+++ example/themes/original/src/components/UserFormDetails/index.tsx
@@ -224,7 +224,7 @@ export const UserFormDetailsUI = (props: any) => {
 												render={() => (
 													<>
 														<OText size={14} lineHeight={21} color={theme.colors.textNormal} weight={'500'} style={{ textTransform: 'capitalize', alignSelf: 'flex-start' }}>
-															{t(field.code.toUpperCase(), field?.name)}
+															{t(field?.code?.toUpperCase(), field?.name)}
 														</OText>
 														<OInput
 															name={field.code}
