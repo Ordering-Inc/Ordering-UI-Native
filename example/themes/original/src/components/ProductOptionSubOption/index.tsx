d3b477738cf5cff41e1c99eced7c9fb24cae9877
Jesus David <jesusdpch00@gmail.com>
2022-04-06 14:13:00 -0400

    [fix] meesage display error max

--- example/themes/original/src/components/ProductOptionSubOption/index.tsx
+++ example/themes/original/src/components/ProductOptionSubOption/index.tsx
@@ -5,7 +5,7 @@ import {
 	ProductOptionSuboption as ProductSubOptionController
 } from 'ordering-components/native'
 import { useTheme } from 'styled-components/native';
-import { StyleSheet } from 'react-native'
+import { StyleSheet, View } from 'react-native'
 
 import {
 	Container,
@@ -52,81 +52,83 @@ export const ProductOptionSubOptionUI = (props: any) => {
 	const disableIncrement = option?.limit_suboptions_by_max ? balance === option?.max : state.quantity === suboption?.max || (!state.selected && balance === option?.max)
 	const price = option?.with_half_option && suboption?.half_price && state.position !== 'whole' ? suboption?.half_price : suboption?.price
 	return (
-		<Container onPress={() => handleSuboptionClick()}>
-			<IconControl disabled={disabled} onPress={() => handleSuboptionClick()}>
-				{((option?.min === 0 && option?.max === 1) || option?.max > 1) ? (
-					state?.selected ? (
-						<OIcon src={theme.images.general.check_act} color={theme.colors.primary} width={16} />
+		<View>
+			<Container onPress={() => handleSuboptionClick()}>
+				<IconControl disabled={disabled} onPress={() => handleSuboptionClick()}>
+					{((option?.min === 0 && option?.max === 1) || option?.max > 1) ? (
+						state?.selected ? (
+							<OIcon src={theme.images.general.check_act} color={theme.colors.primary} width={16} />
+						) : (
+							<OIcon src={theme.images.general.check_nor} color={theme.colors.disabled} width={16} />
+						)
 					) : (
-						<OIcon src={theme.images.general.check_nor} color={theme.colors.disabled} width={16} />
-					)
-				) : (
-					state?.selected ? (
-						<OIcon src={theme.images.general.radio_act} color={theme.colors.primary} width={16} />
-					) : (
-						<OIcon src={theme.images.general.radio_nor} color={theme.colors.disabled} width={16} />
-					)
-				)}
-				<OText size={12} lineHeight={18} color={theme.colors.textSecondary} mLeft={10} style={{ flex: 1 }}>
-					{suboption?.name}
+						state?.selected ? (
+							<OIcon src={theme.images.general.radio_act} color={theme.colors.primary} width={16} />
+						) : (
+							<OIcon src={theme.images.general.radio_nor} color={theme.colors.disabled} width={16} />
+						)
+					)}
+					<OText size={12} lineHeight={18} color={theme.colors.textSecondary} mLeft={10} style={{ flex: 1 }}>
+						{suboption?.name}
+					</OText>
+				</IconControl>
+				<QuantityControl>
+					{option?.allow_suboption_quantity && state?.selected && (
+						<>
+							<Checkbox disabled={disabled || state.quantity === 0} onPress={decrement}>
+								<OIcon
+									src={theme.images.general.minus}
+									width={16}
+									color={state.quantity === 0 || disabled ? theme.colors.disabled : theme.colors.primary}
+								/>
+							</Checkbox>
+							<OText size={12} mLeft={5} mRight={5}>
+								{state.quantity}
+							</OText>
+							<Checkbox disabled={disabled || disableIncrement} onPress={increment}>
+								<OIcon
+									src={theme.images.general.plus}
+									width={16}
+									color={disableIncrement || disabled ? theme.colors.disabled : theme.colors.primary}
+								/>
+							</Checkbox>
+						</>
+					)}
+				</QuantityControl>
+				<PositionControl>
+					{option?.with_half_option && state?.selected && (
+						<>
+							<Circle disabled={disabled} onPress={() => changePosition('left')}>
+								<OIcon
+									src={theme.images.general.half_l}
+									color={state.selected && state.position === 'left' ? theme.colors.primary : '#cbcbcb'}
+									width={16}
+									style={styles.inverse}
+								/>
+							</Circle>
+							<Circle disabled={disabled} onPress={() => changePosition('whole')}>
+								<OIcon
+									src={theme.images.general.half_f}
+									color={state.selected && state.position === 'whole' ? theme.colors.primary : '#cbcbcb'}
+									width={16}
+								/>
+							</Circle>
+							<Circle disabled={disabled} onPress={() => changePosition('right')}>
+								<OIcon
+									src={theme.images.general.half_r}
+									color={state.selected && state.position === 'right' ? theme.colors.primary : '#cbcbcb'}
+									width={16}
+								/>
+							</Circle>
+						</>
+					)}
+				</PositionControl>
+				<OText size={12} lineHeight={18} color={theme.colors.textSecondary}>
+					+ {parsePrice(price)}
 				</OText>
-			</IconControl>
+			</Container>
 			{showMessage && <OText size={10} mLeft={4} mRight={4} style={{ flex: 1, textAlign: 'center' }} color={theme.colors.primary}>{`${t('OPTIONS_MAX_LIMIT', 'Maximum options to choose')}: ${option?.max}`}</OText>}
-			<QuantityControl>
-				{option?.allow_suboption_quantity && state?.selected && (
-					<>
-						<Checkbox disabled={disabled || state.quantity === 0} onPress={decrement}>
-							<OIcon
-								src={theme.images.general.minus}
-								width={16}
-								color={state.quantity === 0 || disabled ? theme.colors.disabled : theme.colors.primary}
-							/>
-						</Checkbox>
-						<OText size={12} mLeft={5} mRight={5}>
-							{state.quantity}
-						</OText>
-						<Checkbox disabled={disabled || disableIncrement} onPress={increment}>
-							<OIcon
-								src={theme.images.general.plus}
-								width={16}
-								color={disableIncrement || disabled ? theme.colors.disabled : theme.colors.primary}
-							/>
-						</Checkbox>
-					</>
-				)}
-			</QuantityControl>
-			<PositionControl>
-				{option?.with_half_option && state?.selected && (
-					<>
-						<Circle disabled={disabled} onPress={() => changePosition('left')}>
-							<OIcon
-								src={theme.images.general.half_l}
-								color={state.selected && state.position === 'left' ? theme.colors.primary : '#cbcbcb'}
-								width={16}
-								style={styles.inverse}
-							/>
-						</Circle>
-						<Circle disabled={disabled} onPress={() => changePosition('whole')}>
-							<OIcon
-								src={theme.images.general.half_f}
-								color={state.selected && state.position === 'whole' ? theme.colors.primary : '#cbcbcb'}
-								width={16}
-							/>
-						</Circle>
-						<Circle disabled={disabled} onPress={() => changePosition('right')}>
-							<OIcon
-								src={theme.images.general.half_r}
-								color={state.selected && state.position === 'right' ? theme.colors.primary : '#cbcbcb'}
-								width={16}
-							/>
-						</Circle>
-					</>
-				)}
-			</PositionControl>
-			<OText size={12} lineHeight={18} color={theme.colors.textSecondary}>
-				+ {parsePrice(price)}
-			</OText>
-		</Container>
+		</View>
 	)
 }
 
