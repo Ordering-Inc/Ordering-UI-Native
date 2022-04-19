fdd6f08e5ca29d5c0c99ae8c3900efe1b9f228a3
Jesus David <jesusdpch00@gmail.com>
2022-04-19 17:49:31 -0400

    [fix] language selector for ios

--- example/src/components/LanguageSelector/index.tsx
+++ example/src/components/LanguageSelector/index.tsx
@@ -1,140 +1,108 @@
-import React, { useState, useEffect } from 'react'
-import { I18nManager, TouchableOpacity, ActivityIndicator, View, Platform } from 'react-native'
-import { useTheme } from 'styled-components/native'
-import {
-  LanguageSelector as LanguageSelectorController,
-  useLanguage
-} from 'ordering-components/native'
-import { StyleSheet } from 'react-native'
-import RNRestart from 'react-native-restart'
-import Picker from 'react-native-country-picker-modal';
+import React, { useState } from 'react'
+import { LanguageSelector as LanguageSelectorController, useOrder } from 'ordering-components/native'
+import { useTheme } from 'styled-components/native';
+import { I18nManager, Platform, StyleSheet, View } from 'react-native'
 
-import { Container, SelectItemBtn, SelectItem } from './styles'
+import RNPickerSelect from 'react-native-picker-select'
+import { Container, DummyContainer } from './styles'
 import { LanguageSelectorParams } from '../../types'
-import { OText } from '../shared'
+import { OIcon } from '../shared'
+import RNRestart from 'react-native-restart'
 
 const LanguageSelectorUI = (props: LanguageSelectorParams) => {
+
   const {
     languagesState,
     currentLanguage,
     handleChangeLanguage,
-    isFromProfile
+    pickerStyle
   } = props
 
-  const theme = useTheme()
-  const [languageState] = useLanguage()
-  const [isOpen, setIsOpen] = useState(false);
-  const [optionSelected, setOptionSelected] = useState<any>(null)
-  let current;
+  const [orderState] = useOrder()
+  const theme = useTheme();
 
-  const styles = StyleSheet.create({
-    itemSelected: {
-      backgroundColor: theme.colors.disabled,
-    },
-    closeBtn: {
-      width: 40,
+  const [language, setLanguage] = useState(currentLanguage)
+
+  const _pickerStyle = StyleSheet.create({
+    inputAndroid: {
+      color: '#000',
+      borderWidth: 1,
+      borderColor: theme.colors.clear,
+      padding: 10,
       height: 40,
+      backgroundColor: theme.colors.disabled,
+      borderRadius: 8
+    },
+    inputIOS: {
+      color: '#000',
+      padding: 10,
+      height: 40,
+      borderWidth: 1,
+      marginStart: 20,
+      borderColor: theme.colors.clear,
+      backgroundColor: theme.colors.clear,
+    },
+    icon: {
+      width: 10,
+      marginTop: 9,
+      marginEnd: 10
+    },
+    placeholder: {
+      color: theme.colors.secundaryContrast
+    },
+    chevronDown: {
+      display: 'none'
+    },
+    chevronUp: {
+      display: 'none'
     }
   })
 
   const _languages = languagesState?.languages?.map((language: any) => {
     return {
-      key: language.code,
-      value: language.code,
-      label: language.name.toUpperCase()
+      value: language?.code,
+      label: language?.name,
+      inputLabel: language?.name
     }
   })
   _languages && _languages.sort((a: any, b: any) =>
-    (a.label > b.label) ? 1 : ((b.label > a.label) ? -1 : 0)
+    (a.content > b.content) ? 1 : ((b.content > a.content) ? -1 : 0)
   )
 
-  const langSelectedObj: any = _languages && _languages.find((item: any) => item.value === currentLanguage) || {}
-
   const changeDirection = async (language: any) => {
-    if(language !== 'ar'){
-      if (I18nManager.isRTL){
+    if (language !== 'ar') {
+      if (I18nManager.isRTL) {
         await I18nManager.forceRTL(false)
         RNRestart.Restart();
       }
     } else {
-      if(!I18nManager.isRTL){
+      if (!I18nManager.isRTL) {
         await I18nManager.forceRTL(true)
         RNRestart.Restart();
       }
     }
   }
 
-  const handlerChangeLanguage = (langCode: any) => {
-    setOptionSelected(langCode)
-    changeDirection(langCode)
-    handleChangeLanguage(langCode)
+  const handlerChangeLanguage = (langCode?: any) => {
+    changeDirection(Platform.OS === 'ios' ? language : langCode)
+    handleChangeLanguage(Platform.OS === 'ios' ? language : langCode)
   }
-
-  useEffect(() => {
-    if (optionSelected === languageState?.language?.code && !languageState.loading) {
-      setIsOpen(false)
-    }
-  }, [languageState])
 
   return (
     <Container>
-      {languagesState?.languages && (
-        <Picker
-          countryCodes={current}
-          visible={isOpen}
-          onClose={() => setIsOpen(false)}
-          withCountryNameButton
-          // @ts-ignore
-          closeButtonStyle={{
-            width: '100%',
-            alignItems: 'flex-end',
-            padding: 10
-          }}
-          closeButtonImageStyle={Platform.OS === 'ios' && styles.closeBtn}
-          renderFlagButton={() => (
-            <>
-              <TouchableOpacity
-                onPress={() => setIsOpen(true)}
-                disabled={_languages.length === 0 || languageState.loading}
-              >
-                <SelectItemBtn isFromProfile={isFromProfile}>
-                  <OText
-                    color={theme.colors.secundaryContrast}
-                    size={14}
-                  >
-                    {langSelectedObj.key.toUpperCase()}
-                  </OText>
-                </SelectItemBtn>
-              </TouchableOpacity>
-            </>
-          )}
-          flatListProps={{
-            keyExtractor: (item: any) => item.value,
-            data: _languages || [],
-            renderItem: ({ item }: any) => (
-              <TouchableOpacity
-                style={langSelectedObj.value === item.value && styles.itemSelected}
-                disabled={langSelectedObj.value === item.value || languageState.loading}
-                onPress={() => handlerChangeLanguage(item.value)}
-              >
-                <SelectItem>
-                  <View style={{ width: 40 }}>
-                    {optionSelected === item.value && languageState.loading && (
-                      <ActivityIndicator size="small" color={theme.colors.primary} />
-                    )}
-                  </View>
-                  <OText
-                    size={14}
-                    style={{ marginRight: 10 }}
-                  >
-                    {item.label}
-                  </OText>
-                </SelectItem>
-              </TouchableOpacity>
-            ),
-          }}
+      {languagesState?.languages ? (
+        <RNPickerSelect
+          onValueChange={(val) => Platform.OS === 'ios' ? setLanguage(val) : handlerChangeLanguage(val)}
+          items={_languages || []}
+          value={Platform.OS === 'ios' ? language : currentLanguage}
+          style={pickerStyle ? pickerStyle : _pickerStyle}
+          useNativeAndroidPickerStyle={false}
+          placeholder={{}}
+          disabled={orderState.loading}
+          onClose={() => Platform.OS === 'ios' ? handlerChangeLanguage() : {}}
+          onDonePress={() => handlerChangeLanguage()}
         />
-      )}
+      ) : <DummyContainer />}
     </Container>
   )
 }
