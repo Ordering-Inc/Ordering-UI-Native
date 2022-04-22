0b09b4f536c203a22adbd72b43ea474dc8843c00
Min <skyandarti@outlook.com>
2022-04-23 04:14:27 +0800

    Update: unnecessary code

--- example/src/pages/ForgotPassword.tsx
+++ example/src/pages/ForgotPassword.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import { Platform } from 'react-native';
 import styled from 'styled-components/native';
-import {
-  ForgotPasswordForm,
-  Container,
-} from '../../themes/original';
+import { ForgotPasswordForm } from '../components/ForgotPasswordForm';
+import { Container } from '../layouts/Container';
 
 const KeyboardView = styled.KeyboardAvoidingView`
   flex: 1;
@@ -14,12 +12,13 @@ export const ForgotPassword = (props: any) => {
   return (
     <KeyboardView
       enabled
-      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+    >
       <Container>
         <ForgotPasswordForm {...props} />
       </Container>
     </KeyboardView>
-  );
-};
+  )
+}
 
 export default ForgotPassword;
--- example/src/pages/Signup.tsx
+++ example/src/pages/Signup.tsx
@@ -1,21 +1,18 @@
 import React from 'react';
-import {
-  SignupForm,
-  Container,
-  _setStoreData,
-  _removeStoreData,
-} from '../../themes/original';
+import { SignupForm } from '../components/SignupForm';
+import { Container } from '../layouts/Container'
 import styled from 'styled-components/native';
 import { useLanguage, useSession } from 'ordering-components/native';
 import { Platform } from 'react-native';
+import { _setStoreData, _removeStoreData } from '../providers/StoreUtil';
 
 const KeyboardView = styled.KeyboardAvoidingView`
   flex: 1;
 `;
 
 export const Signup = (props: any) => {
-  const [, t] = useLanguage();
-  const [, { login }] = useSession();
+  const [, t] = useLanguage()
+  const [, { login }] = useSession()
 
   const signupProps = {
     ...props,
@@ -25,28 +22,27 @@ export const Signup = (props: any) => {
     useSignupByEmail: true,
     notificationState: props.route?.params?.notification_state,
     onNavigationRedirect: (page: string) => {
-      if (!page) {
-        return;
-      }
+      if (!page) return
       props.navigation.navigate(page);
     },
     handleSuccessSignup: (user: any) => {
-      _removeStoreData('isGuestUser');
+      _removeStoreData('isGuestUser')
       if (user?.id) {
         login({
           user,
-          token: user.session.access_token,
-        });
+          token: user.session.access_token
+        })
       }
-    },
-  };
+    }
+  }
 
   _setStoreData('notification_state', props.route?.params?.notification_state);
 
   return (
     <KeyboardView
       enabled
-      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+    >
       <Container>
         <SignupForm {...signupProps} />
       </Container>
