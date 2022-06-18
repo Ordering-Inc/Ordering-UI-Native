2c27290f1261d9e319fa7f0613f12ac139689c9b
Carlos Quiroz <77166972+carlosq-ordering@users.noreply.github.com>
2022-06-17 20:00:36 -0400

    Merge pull request #843 from Ordering-Inc/cart-feedback
    [Fix] Cart feedback

--- example/themes/original/src/components/Checkout/index.tsx
+++ example/themes/original/src/components/Checkout/index.tsx
@@ -250,7 +250,7 @@ const CheckoutUI = (props: any) => {
 
 	useEffect(() => {
 		if (cart?.products?.length === 0) {
-			navigation?.canGoBack() && navigation.goBack();
+			onNavigationRedirect('Business', { store: cart?.business?.slug })
 		}
 	}, [cart?.products])
 
