089f931a5568659e0d0971d66b6bc06583632885
dreamsky2020 <59742254+dreamsky2020@users.noreply.github.com>
2022-06-30 00:08:02 +0800

    [Fix] error

--- example/src/utils/index.tsx
+++ example/src/utils/index.tsx
@@ -450,7 +450,7 @@ export const orderCommentList = (value: string) => {
 
   if (!messages[status]) return null
 
-  const list = messages[status].map((val: any, i: number) => ({ key: i, value: val, content: t(`REJECT_REASON_${val.toUpperCase()}`, val.replaceAll('_', ' ')) }))
+  const list = messages[status].map((val: any, i: number) => ({ key: i, value: val, content: t(`REJECT_REASON_${val.toUpperCase()}`, val.replace(/_/g, ' ')) }))
 
   return { list }
 }
