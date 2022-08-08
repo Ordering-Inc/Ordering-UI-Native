b87b096dd13dc3cd64d92ea0df1759cd3b2c583c
MingXue <mingxue@ordering.co>
2022-08-09 03:17:07 +0800

    resolve conflict

--- example/themes/original/src/components/NavBar/index.tsx
+++ example/themes/original/src/components/NavBar/index.tsx
@@ -62,7 +62,7 @@ const NavBar = (props: Props) => {
 		<Wrapper style={{ paddingTop: props.paddingTop, ...{ flexDirection: props.isVertical ? 'column' : 'row', alignItems: props.isVertical ? 'flex-start' : 'center' }, ...props.style }}>
 			<OButton
 				imgLeftSrc={props.leftImg || theme.images.general.arrow_left}
-				imgLeftStyle={{ width: 30 }}
+				imgLeftStyle={{ width: 18 }}
 				imgRightSrc={null}
 				style={{ ...btnBackArrow, ...props.btnStyle, ...props.isVertical ? (I18nManager.isRTL ? { paddingRight: 0 } : { paddingLeft: 0 }) : {} }}
 				onClick={props?.onActionLeft}
