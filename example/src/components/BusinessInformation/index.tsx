008995ad64c6f8b093e987a931c00e6f14dd990d
dreamsky2020 <59742254+dreamsky2020@users.noreply.github.com>
2022-08-18 22:22:08 +0800

    [Add] validate

--- example/src/components/BusinessInformation/index.tsx
+++ example/src/components/BusinessInformation/index.tsx
@@ -51,8 +51,12 @@ const BusinessInformationUI = (props: BusinessInformationParams) => {
           <GrayBackground>
             <OText size={16} weight='bold'>{t('BUSINESS_DETAILS', 'Business Details')}</OText>
           </GrayBackground>
-          <OText size={14} mBottom={5} mLeft={15} style={{ marginTop: 10 }}>{t('EMAIL', 'Email')}: <OText color={theme.colors.textSecondary}>{businessState?.business?.email}</OText></OText>
-          <OText size={14} mBottom={20} mLeft={15}>{t('CELLPHONE', 'Cellphone')}: <OText color={theme.colors.textSecondary}>{businessState?.business?.cellphone}</OText></OText>
+          {businessState?.business?.email && (
+            <OText size={14} mBottom={5} mLeft={15} style={{ marginTop: 10 }}>{t('EMAIL', 'Email')}: <OText color={theme.colors.textSecondary}>{businessState?.business?.email}</OText></OText>
+          )}
+          {businessState?.business?.cellphone && (
+            <OText size={14} mBottom={20} mLeft={15}>{t('CELLPHONE', 'Cellphone')}: <OText color={theme.colors.textSecondary}>{businessState?.business?.cellphone}</OText></OText>
+          )}
 
           <GrayBackground>
             <OText size={16} weight='bold'>{t('BUSINESS_LOCATION', 'Business Location')}</OText>
