4f734fbf93b41f0228d4f191e0e025fcba1aefa6
Carlos Quiroz <77166972+carlosq-ordering@users.noreply.github.com>
2022-10-24 18:42:57 -0400

    Merge pull request #1073 from Ordering-Inc/professional-feedback
    [Update]: professional

--- example/themes/original/src/components/ServiceForm/index.tsx
+++ example/themes/original/src/components/ServiceForm/index.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react'
 import { useTheme } from 'styled-components/native'
 import { Platform, View, StyleSheet, Dimensions, ScrollView, TouchableOpacity } from 'react-native'
-import { OText, OButton, OModal } from '../shared'
+import { OText, OButton, OModal, OIcon } from '../shared'
 import FastImage from 'react-native-fast-image'
 import IconAntDesign from 'react-native-vector-icons/AntDesign'
 import SelectDropdown from 'react-native-select-dropdown'
@@ -241,13 +241,21 @@ const ServiceFormUI = (props: ServiceFormParams) => {
   return (
     <>
       <Container>
-        <ProfessionalPhoto
-          source={{
-            uri:
-            product?.images ||
-              optimizeImage(theme?.images?.dummies?.product, 'h_250,c_limit'),
-          }}
-        />
+        {!!product?.images ? (
+          <ProfessionalPhoto
+            source={{
+              uri: product?.images
+            }}
+          />
+        ) : (
+          <OIcon
+            src={theme?.images?.dummies?.product}
+            cover={false}
+            style={{ alignSelf: 'center' }}
+            width={200}
+            height={200}
+          />
+        )}
         <InfoWrapper>
           <OText
             size={20}
@@ -295,50 +303,62 @@ const ServiceFormUI = (props: ServiceFormParams) => {
               {t('REQUIRED', 'Required')}
             </OText>
           </View>
-          {currentProfessional && (
-            <TouchableOpacity
-              style={styles.professionalSelect}
-              onPress={() => setIsOpen(true)}
-            >
-              <View style={{ flexDirection: 'row' }}>
-                <FastImage
-                  style={styles.photoStyle}
-                  source={{
-                    uri: optimizeImage(currentProfessional?.photo, 'h_250,c_limit'),
-                    priority: FastImage.priority.normal,
-                  }}
-                  resizeMode={FastImage.resizeMode.cover}
-                />
-                <View style={{ marginLeft: 14 }}>
-                  <OText
-                    size={14}
-                    weight={'400'}
-                    lineHeight={22}
-                  >
-                    {currentProfessional?.name} {currentProfessional?.lastname}
-                  </OText>
-                  <OText
-                    size={12}
-                    weight={'400'}
-                    lineHeight={17}
-                    color={isBusyTime(currentProfessional) ? theme.colors.danger5 : theme.colors.success500}
-                  >
-                    {isBusyTime(currentProfessional)
-                      ? t('BUSY_ON_SELECTED_TIME', 'Busy on selected time')
-                      : t('AVAILABLE', 'Available')
-                    }
-                  </OText>
+          <TouchableOpacity
+            style={styles.professionalSelect}
+            onPress={() => setIsOpen(true)}
+          >
+            {!!currentProfessional ? (
+              <>
+                <View style={{ flexDirection: 'row' }}>
+                  {!!currentProfessional?.photo ? (
+                    <FastImage
+                      style={styles.photoStyle}
+                      source={{
+                        uri: optimizeImage(currentProfessional?.photo, 'h_250,c_limit'),
+                        priority: FastImage.priority.normal,
+                      }}
+                      resizeMode={FastImage.resizeMode.cover}
+                    />
+                  ) : (
+                    <OIcon
+                      src={theme?.images?.general?.user}
+                      cover={false}
+                      style={styles.photoStyle}
+                    />
+                  )}
+                  <View style={{ marginLeft: 14 }}>
+                    <OText
+                      size={14}
+                      weight={'400'}
+                      lineHeight={22}
+                    >
+                      {currentProfessional?.name} {currentProfessional?.lastname}
+                    </OText>
+                    <OText
+                      size={12}
+                      weight={'400'}
+                      lineHeight={17}
+                      color={isBusyTime(currentProfessional) ? theme.colors.danger5 : theme.colors.success500}
+                    >
+                      {isBusyTime(currentProfessional)
+                        ? t('BUSY_ON_SELECTED_TIME', 'Busy on selected time')
+                        : t('AVAILABLE', 'Available')
+                      }
+                    </OText>
+                  </View>
                 </View>
-              </View>
-              <View style={{ marginLeft: 5 }}>
-                <IconAntDesign
-                  name='down'
-                  color={theme.colors.textThird}
-                  size={12}
-                />
-              </View>
-            </TouchableOpacity>
-          )}
+              </>
+            ) : (
+              <OText size={12}>{t('SELECT_PROFESSIONAL', 'Select professional')}</OText>
+            )}
+            <View style={{ marginLeft: 5 }}>
+              <IconAntDesign
+                name='down'
+                color={theme.colors.textThird}
+                size={12}
+              />
+            </View>
+          </TouchableOpacity>
         </ProfessionalWrapper>
         <ScheduleWrapper>
           <View
@@ -448,8 +468,9 @@ const ServiceFormUI = (props: ServiceFormParams) => {
             </CalendarWrapper>
           ) : (
             <OText
-              size={20}
-              style={{ marginBottom: 30 }}
+              size={16}
+              style={{ marginBottom: 30, textAlign: 'center' }}
+              color={theme?.colors?.disabled}
               weight={Platform.OS === 'ios' ? '600' : 'bold'}
             >
               {t('NO_SCHEDULE', 'No schedule')}
@@ -525,21 +546,29 @@ const ServiceFormUI = (props: ServiceFormParams) => {
               {t('ANY_OROFESSIONAL_MEMBER', 'Any professional member')}
             </OText>
           </View>
-          {professionalList?.map((professional: any) => professional?.products?.includes(product?.id) && (
+          {professionalList?.map((professional: any) => (
             <TouchableOpacity
               key={professional?.id}
               style={styles.professionalItem}
               onPress={() => handleChangeProfessional(professional)}
             >
               <View style={{ flexDirection: 'row' }}>
-                <FastImage
-                  style={styles.photoStyle}
-                  source={{
-                    uri: optimizeImage(professional?.photo, 'h_250,c_limit'),
-                    priority: FastImage.priority.normal,
-                  }}
-                  resizeMode={FastImage.resizeMode.cover}
-                />
+                {!!professional?.photo ? (
+                  <FastImage
+                    style={styles.photoStyle}
+                    source={{
+                      uri: optimizeImage(professional?.photo, 'h_250,c_limit'),
+                      priority: FastImage.priority.normal,
+                    }}
+                    resizeMode={FastImage.resizeMode.cover}
+                  />
+                ) : (
+                  <OIcon
+                    src={theme?.images?.general?.user}
+                    cover={false}
+                    style={styles.photoStyle}
+                  />
+                )}
                 <View style={{ marginLeft: 14 }}>
                   <OText
                     size={14}
