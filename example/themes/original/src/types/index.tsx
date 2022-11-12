0ae323d97d2d7d456d13e1863bb2f14e18aae692
Carlos Quiroz <77166972+carlosq-ordering@users.noreply.github.com>
2022-11-12 14:11:49 -0400

    Merge pull request #1103 from Ordering-Inc/business-search-franchise
    [ADD] filter franchise for business search

--- example/themes/original/src/types/index.tsx
+++ example/themes/original/src/types/index.tsx
@@ -651,6 +651,7 @@ export interface BusinessSearchParams {
 	onNavigationRedirect?: any,
 	handleUpdateProducts: any,
 	handleUpdateBusinessList?: any;
+	brandId?: number
 }
 
 export interface NoNetworkParams {
