df155bdeca7a13e165bc95962af3d47245966ccd
dreamsky2020 <59742254+dreamsky2020@users.noreply.github.com>
2022-12-12 21:36:28 -0600

    [Fix] page banner issues

--- example/themes/original/src/components/PageBanner/styles.tsx
+++ example/themes/original/src/components/PageBanner/styles.tsx
@@ -1,8 +1,18 @@
-import styled, { css } from 'styled-components/native'
+import styled from 'styled-components/native'
 
 export const PageBannerWrapper = styled.View`
   margin-horizontal: 40px;
   border-radius: 8px;
   overflow: hidden;
   margin-vertical: 30px;
+  position: relative;
+  flex-direction: row;
+  align-items: center;
+`
+export const ArrowButtonsContainer = styled.View`
+  position: absolute;
+  z-index: 100;
+  flex-direction: row;
+  justify-content: space-between;
+  width: 100%;
 `
