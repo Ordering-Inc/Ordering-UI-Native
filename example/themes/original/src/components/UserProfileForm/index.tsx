eb1a1f345eefc8eac28e22b6ed3e45632ee53146
Carlos Quiroz <77166972+carlosq-ordering@users.noreply.github.com>
2022-12-28 09:54:59 -0400

    Merge pull request #1176 from Ordering-Inc/customer-app-comments
    Customer app comments

--- example/themes/original/src/components/UserProfileForm/index.tsx
+++ example/themes/original/src/components/UserProfileForm/index.tsx
@@ -16,9 +16,8 @@ import { ProfileParams } from '../../types';
 import { UserFormDetailsUI } from '../UserFormDetails';
 
 import { OIcon, OIconButton, OModal } from '../shared';
-import { CenterView } from './styles';
+import { CenterView, Container } from './styles';
 import NavBar from '../NavBar';
-import { Container } from '../../layouts/Container';
 import { VerifyPhone } from '../VerifyPhone'
 import Ionicons from 'react-native-vector-icons/Ionicons'
 import FastImage from 'react-native-fast-image'
@@ -50,11 +49,6 @@ const ProfileUI = (props: ProfileParams) => {
 			shadowRadius: 2,
 			shadowOpacity: 0.2,
 			backgroundColor: theme.colors.white,
-		},
-		pagePadding: {
-			paddingLeft: 40,
-			paddingRight: 40,
-			justifyContent: 'center',
 		},
 		navBarStyle: {
 			paddingLeft: 40,
@@ -284,19 +278,16 @@ const ProfileUI = (props: ProfileParams) => {
 
 	return (
 		<>
-			<Container
-				pt={15}
-				noPadding
-			>
+			<Container>
 				<NavBar
 					title={t('ACCOUNT', 'Account')}
 					titleAlign={'center'}
 					onActionLeft={() => navigation.goBack()}
 					showCall={false}
-					style={{ paddingHorizontal: 40 }}
+					btnStyle={{ paddingLeft: 0 }}
 				/>
 				<KeyboardAvoidingView behavior={Platform.OS == 'ios' ? 'padding' : 'height'} enabled style={{ flex: 1, flexDirection: 'column', justifyContent: 'center' }}>
-					<CenterView style={styles.pagePadding}>
+					<CenterView>
 						<View style={styles.photo}>
 							{user?.photo ? (
 								<FastImage
@@ -319,15 +310,13 @@ const ProfileUI = (props: ProfileParams) => {
 							onClick={() => handleImagePicker()}
 						/>
 					</CenterView>
-					<View style={{ height: 8, marginLeft: -40, marginRight: -40, backgroundColor: theme.colors.backgroundGray100, marginVertical: 32, zIndex: 10 }} />
+					<View style={{ height: 8, backgroundColor: theme.colors.backgroundGray100, marginVertical: 32, zIndex: 10 }} />
 					<Spinner visible={formState?.loading || verifyPhoneState?.loading} />
-					<View style={styles.pagePadding}>
-						<UserFormDetailsUI
-							{...props}
-							isEdit
-							setWillVerifyOtpState={setWillVerifyOtpState}
-						/>
-					</View>
+					<UserFormDetailsUI
+						{...props}
+						isEdit
+						setWillVerifyOtpState={setWillVerifyOtpState}
+					/>
 				</KeyboardAvoidingView>
 			</Container>
 			<OModal
