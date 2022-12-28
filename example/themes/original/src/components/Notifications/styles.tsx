d5694241edc6ecaafacb3a2f93b241197ce0453b
Carlos Quiroz <77166972+carlosq-ordering@users.noreply.github.com>
2022-12-28 17:04:18 -0400

    Merge pull request #1178 from Ordering-Inc/app-crash
    [Fix] app crash

--- example/themes/original/src/components/Notifications/styles.tsx
+++ example/themes/original/src/components/Notifications/styles.tsx
@@ -1,12 +1,15 @@
 import styled from 'styled-components/native'
 
-export const ContainerButtons = styled.View`
-    margin-vertical: 50px
+export const Container = styled.ScrollView`
+    position: relative;
+    flex: 1;
+	  padding: 20px;
 `
 
 export const NotificationsGroupSwitchWrapper = styled.View`
   flex-grow: 1;
   justify-content: space-between;
+	padding: 0 20px;
 `
 
 export const SwitchWrapper = styled.View`
