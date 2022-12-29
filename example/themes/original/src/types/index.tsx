802e5e0bf517dd2ea843ff9701da7319c56dad6d
Carlos Quiroz <77166972+carlosq-ordering@users.noreply.github.com>
2022-12-29 16:36:35 -0400

    Merge pull request #1181 from Ordering-Inc/view-order-button
    [Update] show view order button with multi business checkout enabled

--- example/themes/original/src/types/index.tsx
+++ example/themes/original/src/types/index.tsx
@@ -251,6 +251,7 @@ export interface BusinessProductsListingParams {
 	handleChangeProfessionalSelected?: any;
 	handleUpdateProfessionals?: any;
 	onBusinessClick?: any;
+	onNavigationRedirect?: any;
 }
 export interface BusinessBasicInformationParams {
 	navigation?: any;
@@ -532,7 +533,6 @@ export interface FloatingButtonParams {
 	handleClick?: any;
 	handleEmpty?: any;
 	iosBottom?: number
-	hideButton?: boolean
 }
 export interface MomentOptionParams {
 	navigation: any;
