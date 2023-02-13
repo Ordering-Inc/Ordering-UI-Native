5b7662eae72e73952776a2bb158abf862c5e6c05
Carlos Quiroz <77166972+carlosq-ordering@users.noreply.github.com>
2023-02-13 08:26:16 -0400

    Merge pull request #1267 from Ordering-Inc/place-order-bug-3
    [FIX] alert shows

--- example/themes/original/src/components/Checkout/index.tsx
+++ example/themes/original/src/components/Checkout/index.tsx
@@ -744,6 +744,7 @@ const CheckoutUI = (props: any) => {
 									setIsOpen(false)
 									handlePlaceOrder(null, true)
 								}}
+								setIsOpen={setIsOpen}
 							/>
 						</View>
 					</OModal>
