410bc46dbc8fef6d7ddffe985e43fa4be1781985
carlosg-ordering <carlos.g@ordering.co>
2023-02-21 14:26:38 -0600

    missed change

--- example/themes/original/src/components/AddressForm/index.tsx
+++ example/themes/original/src/components/AddressForm/index.tsx
@@ -532,7 +532,7 @@ const AddressFormUI = (props: AddressFormParams) => {
 				/>
 			</View>
 			<TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-				<AddressFormContainer style={{ height: HEIGHT_SCREEN * .80, overflow: 'scroll' }}>
+				<AddressFormContainer style={{ height: HEIGHT_SCREEN * .78, overflow: 'scroll' }}>
 					<View>
 						<FormInput>
 							<AutocompleteInput>
