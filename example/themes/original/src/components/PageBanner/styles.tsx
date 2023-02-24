e785f4fd33f42affb80a681fc9922ca4876356d4
dreamsky2020 <59742254+dreamsky2020@users.noreply.github.com>
2023-02-24 05:06:52 -0600

    remove space

--- example/themes/original/src/components/PageBanner/styles.tsx
+++ example/themes/original/src/components/PageBanner/styles.tsx
@@ -2,7 +2,6 @@ import styled from 'styled-components/native'
 
 export const PageBannerWrapper = styled.View`
   overflow: hidden;
-  margin-vertical: 30px;
   position: relative;
   flex-direction: row;
   align-items: center;
