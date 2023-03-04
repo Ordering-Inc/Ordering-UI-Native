a8eb93345853fe78c52bfa40004a9676a6452aa9
Carlos Quiroz <carloseqv29596@gmail.com>
2023-03-03 21:03:48 -0400

    [FIX] delivery fee improve

--- example/themes/original/src/components/CartContent/index.tsx
+++ example/themes/original/src/components/CartContent/index.tsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import { View } from 'react-native';
-import { useLanguage, useConfig } from 'ordering-components/native';
+import { useLanguage, useConfig, useUtils } from 'ordering-components/native';
 import { useTheme } from 'styled-components/native';
-import { CCContainer, CCNotCarts, CCList, CheckoutAction } from './styles';
+import { CCContainer, CCNotCarts, CCList, CheckoutAction, ChCartsTotal } from './styles';
 
 import { Cart } from '../Cart';
 import { OButton, OText } from '../shared';
@@ -19,11 +19,17 @@ export const CartContent = (props: any) => {
 	const theme = useTheme();
 	const [, t] = useLanguage()
 	const [{ configs }] = useConfig()
+	const [{ parsePrice }] = useUtils();
 	const [isCartsLoading, setIsCartsLoading] = useState(false)
 
 	const isChewLayout = theme?.header?.components?.layout?.type === 'chew'
 	const isMultiCheckout = configs?.checkout_multi_business_enabled?.value === '1'
 	const cartsAvailable: any = Object.values(carts)?.filter((cart: any) => cart?.valid && cart?.status !== 2)
+
+	const totalCartsPrice = cartsAvailable?.length && cartsAvailable.reduce((total: any, cart: any) => { return total + cart?.total }, 0)
+	const totalCartsFee = cartsAvailable?.length && cartsAvailable
+		?.filter((cart: any) => cart?.status !== 1 && cart?.valid && cart?.products?.length)
+		?.reduce((total: any, cart: any) => { return total + (cart?.delivery_price_with_discount) }, 0)
 
 	const handleCheckoutRedirect = () => {
 		if (cartsAvailable.length === 1) {
@@ -80,6 +86,9 @@ export const CartContent = (props: any) => {
 										isMultiCheckout={isMultiCheckout}
 										hideUpselling
 										businessConfigs={cart?.business?.configs}
+										hideCouponInput={configs?.multi_business_checkout_coupon_input_style?.value === 'group'}
+                    hideDeliveryFee={configs?.multi_business_checkout_show_combined_delivery_fee?.value === '1'}
+										hideDriverTip={configs?.multi_business_checkout_show_combined_driver_tip?.value === '1'}
 									/>
 									<View style={{ height: 8, backgroundColor: theme.colors.backgroundGray100, marginHorizontal: -40, marginTop: 20 }} />
 								</>
@@ -87,18 +96,51 @@ export const CartContent = (props: any) => {
 						</CCList>
 					))}
 					{isMultiCheckout && (
-						<CheckoutAction style={{ marginTop: 0 }}>
-							<OButton
-								text={t('CHECKOUT', 'Checkout')}
-								bgColor={!cartsAvailable.length ? theme.colors.secundary : theme.colors.primary}
-								isDisabled={!cartsAvailable.length}
-								borderColor={theme.colors.primary}
-								imgRightSrc={null}
-								textStyle={{ color: 'white', textAlign: 'center', flex: 1 }}
-								onClick={() => handleCheckoutRedirect()}
-								style={{ width: '100%', flexDirection: 'row', justifyContent: 'center', borderRadius: 7.6, shadowOpacity: 0 }}
-							/>
-						</CheckoutAction>
+						<>
+							{!!cartsAvailable.length && (
+								<ChCartsTotal>
+									{!!totalCartsFee && configs?.multi_business_checkout_show_combined_delivery_fee?.value === '1' && (
+                    <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+                      <OText size={14} lineHeight={24} color={theme.colors.textNormal} weight={'400'}>
+                        {t('TOTAL_DELIVERY_FEE', 'Total delivery fee')}
+                      </OText>
+                      <OText size={14} lineHeight={24} color={theme.colors.textNormal} weight={'400'}>
+                        {parsePrice(totalCartsFee)}
+                      </OText>
+                    </View>
+                  )}
+                  {cartsAvailable.reduce((sum: any, cart: any) => sum + cart?.driver_tip, 0) > 0 &&
+                    configs?.multi_business_checkout_show_combined_driver_tip?.value === '1' && (
+                    <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+                      <OText size={14} lineHeight={24} color={theme.colors.textNormal} weight={'400'}>
+                        {t('DRIVER_TIP', 'Driver tip')}
+                      </OText>
+                      <OText size={14} lineHeight={24} color={theme.colors.textNormal} weight={'400'}>
+                        {parsePrice(cartsAvailable.reduce((sum: any, cart: any) => sum + cart?.driver_tip, 0))}
+                      </OText>
+                    </View>
+                  )}
+                  <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+                    <OText size={16} lineHeight={24} color={theme.colors.textNormal} weight={'500'}>
+                      {t('TOTAL_FOR_ALL_CARTS', 'Total for all Carts')}
+                    </OText>
+                    <OText size={16} lineHeight={24} color={theme.colors.textNormal} weight={'500'}>{parsePrice(totalCartsPrice)}</OText>
+                  </View>
+								</ChCartsTotal>
+							)}
+							<CheckoutAction style={{ marginTop: 0 }}>
+								<OButton
+									text={t('CHECKOUT', 'Checkout')}
+									bgColor={!cartsAvailable.length ? theme.colors.secundary : theme.colors.primary}
+									isDisabled={!cartsAvailable.length}
+									borderColor={theme.colors.primary}
+									imgRightSrc={null}
+									textStyle={{ color: 'white', textAlign: 'center', flex: 1 }}
+									onClick={() => handleCheckoutRedirect()}
+									style={{ width: '100%', flexDirection: 'row', justifyContent: 'center', borderRadius: 7.6, shadowOpacity: 0 }}
+								/>
+							</CheckoutAction>
+						</>
 					)}
 				</>
 			)}
