9e195a9679f4092da0bffa3749f25149d65b1573
Israel Rojas <israelrojas626@gmail.com>
2023-03-05 10:43:22 -0400

    [Fix] reorder button not showing in finished orders

--- example/themes/original/src/components/SingleOrderCard/index.tsx
+++ example/themes/original/src/components/SingleOrderCard/index.tsx
@@ -286,7 +286,7 @@ const SingleOrderCardUI = (props: SingleOrderCardParams) => {
                         </OText>
                       </TouchableOpacity>
                     )}
-                  {order.cart && !hideReorderButton && (
+                  {!hideReorderButton && (
                     <OButton
                       text={t('REORDER', 'Reorder')}
                       imgRightSrc={''}
