7d96f83cca7b9e8045729b2e4838b2ff187b3e10
Israel Rojas <israelrojas626@gmail.com>
2023-03-22 12:55:51 -0400

    [Update] chew title and arrow left with wallet bottom bar config

--- example/themes/original/src/components/Wallets/index.tsx
+++ example/themes/original/src/components/Wallets/index.tsx
@@ -3,6 +3,7 @@ import { Pressable, StyleSheet, View, ScrollView, TouchableOpacity, Platform } f
 import { useTheme } from 'styled-components/native'
 import { Fade, Placeholder, PlaceholderLine } from 'rn-placeholder';
 import FastImage from 'react-native-fast-image'
+import NavBar from '../NavBar'
 import {
   WalletList,
   useLanguage,
@@ -119,61 +120,29 @@ const WalletsUI = (props: any) => {
 
   return (
     <>
-      <Container>
+      <Container
+        pdng={Platform.OS === 'ios' ? '10px' : '0'}
+      >
         <Header>
           <View style={{
             ...{
               width: '100%',
               display: 'flex',
               flexDirection: hideWalletsTheme ? 'column' : 'row',
-              alignItems: 'center',
-              marginTop: 30,
-              justifyContent: 'space-between',
-              alignContent: 'center'
+              justifyContent: hideWalletsTheme ? 'flex-start' : 'space-between',
+              alignItems: hideWalletsTheme ? 'flex-start' : 'center',
+              marginTop: hideWalletsTheme ? 0 : 10,
             },
           }}>
-            <View style={{
-              ...{
-                width: hideWalletsTheme ? '100%' : 'auto',
-                display: 'flex',
-                flexDirection: 'row',
-                alignItems: 'center',
-                justifyContent: 'space-between',
-                alignContent: 'center'
-              },
-            }}>
-              {hideWalletsTheme && (
-                <OButton
-                  imgLeftStyle={{ width: 18 }}
-                  imgRightSrc={null}
-                  style={{
-                    borderWidth: 0,
-                    width: 26,
-                    height: 26,
-                    backgroundColor: '#FFF',
-                    borderColor: '#FFF',
-                    shadowColor: '#FFF',
-                    paddingLeft: 0,
-                    paddingRight: 0,
-                  }}
-                  onClick={goToBack}
-                  icon={AntDesignIcon}
-                  iconProps={{
-                    name: 'arrowleft',
-                    size: 26
-                  }}
-                />
-              )}
-              <OText
-                size={20}
-                style={{
-                  color: theme.colors.textNormal,
-                }}
-                weight={Platform.OS === 'ios' ? '600' : 'bold'}
-              >
-                {t('WALLETS', 'Wallets')}
-              </OText>
-            </View>
+            <NavBar
+              title={t('WALLETS', 'Wallets')}
+              titleAlign={'center'}
+              onActionLeft={goToBack}
+              showCall={false}
+              paddingTop={10}
+              btnStyle={{ paddingLeft: 0 }}
+              hideArrowLeft={!hideWalletsTheme}
+            />
             {isChewLayout && (
               <OButton
                 text={t('WALLET_HISTORY', 'Wallet history')}
