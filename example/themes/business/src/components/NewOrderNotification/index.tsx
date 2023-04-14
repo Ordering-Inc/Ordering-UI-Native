7af23f93e40cf9d257b6dd08232e037f5cd5eee7
Israel Rojas <israelrojas626@gmail.com>
2023-04-14 15:05:46 -0400

    [Fix] AL order id

--- example/themes/business/src/components/NewOrderNotification/index.tsx
+++ example/themes/business/src/components/NewOrderNotification/index.tsx
@@ -76,7 +76,11 @@ const NewOrderNotificationUI = (props: any) => {
     }, 2500)
   }
 
+  console.log('currentEvent', currentEvent)
+
   const handleEventNotification = async (evtType: number, value: any) => {
+    console.log('evtType', evtType)
+    console.log('value', value)
     if (value?.driver) {
       try {
         const location = await getCurrentLocation()
@@ -97,7 +101,7 @@ const NewOrderNotificationUI = (props: any) => {
     if (evtType === 3 || value.author_id === user.id) return
     setTimeout(() => handlePlayNotificationSound({
       evt: evtType,
-      orderId: value?.driver ? value?.order_id : evtList[evtType].event === 'messages' ? value?.order?.id : value?.id
+      orderId: value?.driver ? value?.order_id : evtList[evtType].event === 'messages' ? value?.order?.id : value?.order_id
     }), 1000)
   }
 
