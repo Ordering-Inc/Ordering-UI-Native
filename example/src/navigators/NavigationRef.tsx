207713bee726b06ec3840f3c83ff8e0490b0f3f0
Carlos Quiroz <carlos.q@ordering.co>
2023-05-15 18:44:25 -0400

    [FEAT] delete unused code

--- example/src/navigators/NavigationRef.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-
-export const navigationRef: any = React.createRef();
-
-export const navigate = (name: string, params: any) => {
-  navigationRef.current?.navigate(name, params);
-}
