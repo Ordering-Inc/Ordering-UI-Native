64037603945e94f37a6b73ec2a8411a5814ae8d4
Israel Rojas <israelrojas626@gmail.com>
2023-06-16 15:48:02 -0400

    modal date picker

--- example/themes/original/src/components/DatePicker/index.tsx
+++ example/themes/original/src/components/DatePicker/index.tsx
@@ -1,18 +1,32 @@
 import React from 'react';
 import DatePicker from 'react-native-date-picker'
 import { DateContainer } from './styles';
+import { useLanguage } from 'ordering-components/native';
 
 export const DatePickerUI = (props: any) => {
 	const {
 		birthdate,
-		handleChangeDate,
 		onConfirm,
-		onCancel
+		onCancel,
+		open,
 	} = props;
+
+	const [, t] = useLanguage();
 
 	return (
 		<DateContainer>
-			<DatePicker mode="date" date={birthdate ? new Date(birthdate) : new Date()} onDateChange={handleChangeDate} onConfirm={date => onConfirm(date)} onCancel={onCancel} />
+			<DatePicker
+				modal
+				mode="date"
+				open={open}
+				title={t('SELECT_A_DATE', 'Select a date')}
+				confirmText={t('CONFIRM', 'Confirm')}
+				cancelText={t('CANCEL', 'Cancel')}
+				date={birthdate ? new Date(birthdate) : new Date()}
+				onConfirm={date => onConfirm(date)}
+				onCancel={onCancel}
+				maximumDate={new Date()}
+			/>
 		</DateContainer>
 	);
 };
