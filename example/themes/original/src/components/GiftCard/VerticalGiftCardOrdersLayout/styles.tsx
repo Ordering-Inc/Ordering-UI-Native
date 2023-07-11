d8617e6cc03619105c3b7e64d47d7e0582f7c392
dreamsky2020 <59742254+dreamsky2020@users.noreply.github.com>
2023-07-11 05:41:21 -0600

    [Fix] style issue

--- example/themes/original/src/components/GiftCard/VerticalGiftCardOrdersLayout/styles.tsx
+++ example/themes/original/src/components/GiftCard/VerticalGiftCardOrdersLayout/styles.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components/native';
 export const ProductsListContainer = styled.View`  
 `
 export const WrappButton = styled.View`
-  margin-bottom: 24;
+  margin-bottom: 24px;
 `
 export const SingleGiftCardWrapper = styled.View`  
 `
