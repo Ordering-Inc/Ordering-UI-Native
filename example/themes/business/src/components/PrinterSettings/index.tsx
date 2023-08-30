baae8143ce353e811e3c01c4f61bbc144fcc76c5
Carlos Quiroz <carloseqv29596@gmail.com>
2023-08-30 02:10:15 -0400

    [FEAT] multiprinter select

--- example/themes/business/src/components/PrinterSettings/index.tsx
+++ example/themes/business/src/components/PrinterSettings/index.tsx
@@ -2,14 +2,13 @@ import React, { useEffect, useState } from 'react'
 import { StyleSheet, TouchableOpacity, View, Dimensions } from 'react-native'
 import SimpleLineIcons from 'react-native-vector-icons/SimpleLineIcons'
 import FeatherIcon from 'react-native-vector-icons/Feather'
-import MCIcons from 'react-native-vector-icons/MaterialCommunityIcons'
 import { useTheme } from 'styled-components/native'
 import ToggleSwitch from 'toggle-switch-react-native';
 import { useLanguage } from 'ordering-components/native'
 
 import { _setStoreData, _retrieveStoreData } from '../../providers/StoreUtil'
-import { Container, ContainerList, EnabledAutoPrint } from './styles'
-import { OText, OIcon, OModal } from '../shared'
+import { Container, ContainerList, EnabledAutoPrint, NoPrintersContainer } from './styles'
+import { OText, OIcon, OModal, OButton } from '../shared'
 import { PrinterEdition } from '../PrinterEdition'
 
 export const PrinterSettings = (props: any) => {
@@ -17,9 +16,10 @@ export const PrinterSettings = (props: any) => {
 
   const [printers, setPrinters] = useState<any>({ list: [] })
   const [autoPrintEnabled, setAutoPrintEnabled] = useState<boolean>(false)
-  const [openModal, setOpenModal] = useState({ open: false, data: null })
+  const [openModal, setOpenModal] = useState<any>({ open: false, data: null })
 
   const WIDTH_SCREEN = Dimensions.get('window').width
+  const HEIGHT_SCREEN = Dimensions.get('window').height
 
   const [, t] = useLanguage()
   const theme = useTheme()
@@ -40,7 +40,7 @@ export const PrinterSettings = (props: any) => {
     wIconContainer: {
       flexDirection: 'row',
       alignItems: 'center',
-      width: WIDTH_SCREEN - 60 - 10 - 26
+      width: WIDTH_SCREEN - 40 - 22 - 22 - 10, // screen - margin - icon - icon - marginIcon
     },
     wrapperContainer: {
       flexDirection: 'column',
@@ -54,9 +54,6 @@ export const PrinterSettings = (props: any) => {
       height: 40,
       borderWidth: 1,
       borderRadius: 8,
-    },
-    label: {
-      color: theme.colors.textGray
     },
     btnBackArrow: {
       borderWidth: 0,
@@ -74,40 +71,28 @@ export const PrinterSettings = (props: any) => {
       flexDirection: 'row',
       minHeight: 33,
     },
+    addButtonText: {
+      color: theme.colors.inputTextColor,
+      fontSize: 16,
+    },
+    addButton: {
+      height: 40,
+      marginTop: 10,
+      borderRadius: 8,
+      marginRight: 10,
+      marginBottom: 10,
+    },
   })
-
-  const printerList = [
-    { model: 'mPOP', emulation: 'StarPRNT', portName1: 'BT:mPOP', type: 1, ip: '', printMode: 'append' },
-    { model: 'FVP10', emulation: 'StarLine', portName1: 'BT:FVP10', type: 1, ip: '', printMode: 'append' },
-    { model: 'TSP100', emulation: 'StarGraphic', portName1: 'BT:TSP100', type: 1, ip: '', printMode: 'appendBitmapText' },
-    { model: 'TSP100IV', emulation: 'StarLine', portName1: 'BT:TSP100iv', type: 1, ip: '', printMode: 'append' },
-    { model: 'TSP65011', emulation: 'StarLine', portName1: 'BT:TSP65011', type: 1, ip: '', printMode: 'append' },
-    { model: 'TSP7001', emulation: 'StarLine', portName1: 'BT:TSP7001', type: 1, ip: '', printMode: 'append' },
-    { model: 'TSP80011', emulation: 'StarLine', portName1: 'BT:TSP80011', type: 1, ip: '', printMode: 'append' },
-    { model: 'SP700', emulation: 'StarDotImpact', portName1: 'BT:SP700', type: 1, ip: '', printMode: 'append' },
-    { model: 'SM-S210i', emulation: 'EscPosMobile', portName1: 'BT:SMS210i', type: 1, ip: '', printMode: 'append' },
-    { model: 'SM-S220i', emulation: 'EscPosMobile', portName1: 'BT:SMS220i', type: 1, ip: '', printMode: 'append' },
-    { model: 'SM-S230i', emulation: 'EscosMobile', portName1: 'BT:SMS230i', type: 1, ip: '', printMode: 'append' },
-    { model: 'SM-T300i/T300', emulation: 'EscPosMobile', portName1: 'BT:SMT300i/T300', type: 1, ip: '', printMode: 'append' },
-    { model: 'SM-T400i', emulation: 'EscosMobile', portName1: 'BT:SMT400i', type: 1, ip: '', printMode: 'append' },
-    { model: 'SM-L200', emulation: 'StarPRNT', portName1: 'BT:SML200', type: 1, ip: '', printMode: 'append' },
-    { model: 'SM-L300', emulation: 'StarPRNT', portName1: 'BT:SML300', type: 1, ip: '', printMode: 'append' },
-    { model: 'BSC10', emulation: 'EscPos', portName1: 'BT:BSC10', type: 1, ip: '', printMode: 'append' },
-    { model: 'SM-S210i StarPRNT', emulation: 'StarPRNT', portName1: 'BT:SMS210i', type: 1, ip: '', printMode: 'append' },
-    { model: 'SM-S220i StarPRNT', emulation: 'StarPRNT', portName1: 'BT:SMS220i', type: 1, ip: '', printMode: 'append' },
-    { model: 'SM-S230i StarPRNT', emulation: 'StarPRNT', portName1: 'BT:SMS230i', type: 1, ip: '', printMode: 'append' },
-    { model: 'SM-T300i/T300 StarPRNT', emulation: 'StarPRNT', portName1: 'BT:SMT300i', type: 1, ip: '', printMode: 'append' },
-    { model: 'SM-T400i StarPRNT', emulation: 'StarPRNT', portName1: 'BT:SMT400i', type: 1, ip: '', printMode: 'append' },
-  ]
 
   const goToBack = () => navigation?.canGoBack() && navigation.goBack()
 
-  const handleClick = async ({ item, type, ip, edit }: any) => {
+  const handleClick = async ({ item, type, ip, edit, isAdd, index }: any) => {
     let _item = item
+    const action = edit || isAdd
     let _printers = printers.list
-    const idx = _printers.findIndex((p: any) => p.model === _item.model)
+    const idx = index ?? _printers.findIndex((p: any) => p.model === _item.model)
 
-    if (idx !== -1 && !edit) {
+    if (idx !== -1 && !action) {
       _printers.splice(idx, 1);
     } else {
       const _currentPrinter = _printers.find((p: any) => p.model === _item?.model)
@@ -154,43 +139,50 @@ export const PrinterSettings = (props: any) => {
       <OText size={24} style={{ paddingTop: 0 }}>
         {t('PRINTER_SETTINGS', 'Printer Settings')}
       </OText>
-      <EnabledAutoPrint>
-        <View style={{ flex: 1 }}>
-          <OText
-            numberOfLines={2}
-            adjustsFontSizeToFit
-            style={{ ...styles.label, paddingHorizontal: 0 }}>
-            {t('AUTO_PRINT_AFTER_ACCEPTING_ORDER', 'Auto print after accepting order')}
-          </OText>
-        </View>
-        <ToggleSwitch
-          isOn={autoPrintEnabled}
-          onColor={theme.colors.primary}
-          offColor={theme.colors.offColor}
-          size="small"
-          onToggle={() => handleAutoPrint()}
-          animationSpeed={200}
+      {!!printers.list.length && (
+        <EnabledAutoPrint>
+          <View style={{ flex: 1 }}>
+            <OText
+              numberOfLines={2}
+              adjustsFontSizeToFit
+              color={theme.colors.textGray}
+              style={{ paddingHorizontal: 0 }}>
+              {t('AUTO_PRINT_AFTER_ACCEPTING_ORDER', 'Auto print after accepting order')}
+            </OText>
+          </View>
+          <ToggleSwitch
+            isOn={autoPrintEnabled}
+            onColor={theme.colors.primary}
+            offColor={theme.colors.offColor}
+            size="small"
+            onToggle={() => handleAutoPrint()}
+            animationSpeed={200}
+          />
+        </EnabledAutoPrint>
+      )}
+      {!!printers.list.length && (
+        <OButton
+          text={t('ADD_PRINTER', 'Add printer')}
+          imgRightSrc={null}
+          textStyle={styles.addButtonText}
+          style={styles.addButton}
+          bgColor={theme.colors.primary}
+          borderColor={theme.colors.primary}
+          onClick={() => setOpenModal({ open: true, data: null })}
         />
-      </EnabledAutoPrint>
+      )}
       <View>
-        {printerList.map((item: any, i: number) => (
+        {printers.list.map((item: any, i: number) => (
           <ContainerList
             key={i}
             activeOpacity={1}
-            onPress={() => handleClick({ item })}
           >
             <View style={styles.wrapperContainer}>
               <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                <MCIcons
-                  name={printers.list.find((p: any) => p?.model === item.model) ? "checkbox-marked" : "checkbox-blank-outline"}
-                  size={26}
-                  color={theme.colors.primary}
-                  onPress={() => handleClick({ item })}
-                />
                 <TouchableOpacity
                   activeOpacity={1}
                   style={styles.wIconContainer}
-                  onPress={() => handleClick({ item })}
+                  onPress={() => setOpenModal({ open: true, data: { ...item, index: i} })}
                 >
                   <SimpleLineIcons
                     name='printer'
@@ -198,55 +190,74 @@ export const PrinterSettings = (props: any) => {
                     size={18}
                     style={{
                       ...styles.icons,
-                      color: printers.list.find((p: any) => p?.model === item.model)
-                        ? theme.colors.primary
-                        : theme.colors.textGray
+                      color: theme.colors.primary
                     }}
                   />
                   <OText
                     size={18}
-                    color={printers.list.find((p: any) => p?.model === item.model)
-                      ? theme.colors.primary
-                      : theme.colors.textGray
-                    }
+                    color={theme.colors.primary}
                   >
-                    {item.model}
+                    {item?.nickname ?? item.model}
                   </OText>
                 </TouchableOpacity>
-                {!!printers.list.find((p: any) => p?.model === item.model) && (
-                  <TouchableOpacity
-                    activeOpacity={1}
-                    onPress={() => setOpenModal({
-                      open: true,
-                      data: printers.list.find((p: any) => p?.model === item.model)
-                    })}
-                    style={{ width: 40 }}
-                  >
-                    <FeatherIcon
-                      name='edit'
-                      size={20}
-                      style={styles.icons}
-                    />
-                  </TouchableOpacity>
-                )}
+                <TouchableOpacity
+                  activeOpacity={1}
+                  onPress={() => setOpenModal({ open: true, data: { ...item, index: i} })}
+                >
+                  <FeatherIcon
+                    name='edit'
+                    size={22}
+                  />
+                </TouchableOpacity>
+                <TouchableOpacity
+                  activeOpacity={1}
+                  onPress={() => handleClick({ item })}
+                  style={{ marginLeft: 10 }}
+                >
+                  <FeatherIcon
+                    name='trash-2'
+                    size={22}
+                    color={theme.colors.danger500}
+                  />
+                </TouchableOpacity>
               </View>
             </View>
           </ContainerList>
         ))}
+        {!printers.list.length && (
+          <NoPrintersContainer style={{ height: HEIGHT_SCREEN/2 }}>
+            <OText
+              size={20}
+              color={theme.colors.textGray}
+              numberOfLines={2}
+              adjustsFontSizeToFit
+              style={{ paddingHorizontal: 0, opacity: 0.4 }}
+            >
+              {t('NO_PRINTERS_CONFIGURED', 'No printers configured')}
+            </OText>
+            <OButton
+              text={t('ADD_PRINTER', 'Add printer')}
+              imgRightSrc={null}
+              textStyle={styles.addButtonText}
+              style={styles.addButton}
+              bgColor={theme.colors.primary}
+              borderColor={theme.colors.primary}
+              onClick={() => setOpenModal({ open: true, data: null })}
+            />
+          </NoPrintersContainer>
+        )}
       </View>
       <OModal
         hideIcons
         entireModal
         customClose
         open={openModal.open}
-        style={{
-          paddingTop: 0,
-          marginTop: 0
-        }}
+        style={{ paddingTop: 0, marginTop: 0 }}
         onClose={() => setOpenModal({ open: false, data: null })}
       >
         <PrinterEdition
           printer={openModal.data}
+          printersList={printers}
           handleChangePrinter={handleClick}
           onClose={() => setOpenModal({ open: false, data: null })}
         />
