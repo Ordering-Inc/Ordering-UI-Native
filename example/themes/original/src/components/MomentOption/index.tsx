f381cbe05195e04e8822e2e870a14ca05ac8743d
YisusJe <jesus@ordering.co>
2023-11-20 17:55:20 -0400

    [FIX] catering times

--- example/themes/original/src/components/MomentOption/index.tsx
+++ example/themes/original/src/components/MomentOption/index.tsx
@@ -321,7 +321,7 @@ const MomentOptionUI = (props: MomentOptionParams) => {
 				}
 			}))
 		}
-	}, [dateSelected, JSON.stringify(hoursList), JSON.stringify(datesWhitelist), cateringPreorder, JSON.stringify(business)])
+	}, [dateSelected, JSON.stringify(hoursList), JSON.stringify(datesWhitelist), JSON.stringify(business)])
 
 	useEffect(() => {
 		setLocalMoment(moment, t)
