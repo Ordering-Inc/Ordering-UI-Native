f6e29aa50e38d0a67fede33a25149f1497bb118c
Carlos Quiroz <77166972+carlosq-ordering@users.noreply.github.com>
2024-05-30 11:31:56 -0400

    Merge pull request #1774 from Ordering-Inc/guest-product-bug
    [FIX] guest flow of adding product

--- example/themes/original/src/components/Home/index.tsx
+++ example/themes/original/src/components/Home/index.tsx
@@ -1,22 +1,48 @@
-import React, { useEffect } from 'react';
-import { useLanguage, useOrder, useConfig } from 'ordering-components/native';
+import React from 'react';
+import { useLanguage, useOrder, useConfig, useSession, useApi, ToastType, useToast } from 'ordering-components/native';
 import { useTheme } from 'styled-components/native';
 import { StyleSheet, View } from 'react-native';
 import { OButton, OIcon, OText } from '../shared';
 import { LanguageSelector } from '../LanguageSelector';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import { useWindowDimensions, Platform } from 'react-native';
+import uuid from 'react-native-uuid';
 
 export const Home = (props: any) => {
 	const { onNavigationRedirect, businessSlug } = props;
 	const { width, height } = useWindowDimensions();
 	const [, t] = useLanguage();
-	const [orderState] = useOrder();
+	const [orderState, { handleOrderStateLoading, setStateInitialValues }] = useOrder();
 	const [{ configs }] = useConfig()
+	const [, { login }] = useSession()
+	const [ordering] = useApi()
+	const [, { showToast }] = useToast()
 
 	const theme = useTheme();
-	const unaddressedTypes = configs?.unaddressed_order_types_allowed?.value.split('|').map((value: any) => Number(value)) || []
-	const isAllowUnaddressOrderType = unaddressedTypes.includes(orderState?.options?.type)
+
+	const handleCreateGuestUser = async (values: any) => {
+		try {
+			await handleOrderStateLoading(true)
+			const { content: { error, result } } = await ordering.users().save(values)
+			if (!error) {
+				await login({
+					user: result,
+					token: result.session?.access_token
+				})
+			} else {
+				showToast(ToastType.Error, t('ERROR_CREATING_GUEST_USER', 'Error creating guest users'))
+			}
+			await handleOrderStateLoading(false)
+		} catch (err) {
+			await handleOrderStateLoading(false)
+			showToast(ToastType.Error, t('ERROR_CREATING_GUEST_USER', 'Error creating guest users'))
+		}
+	}
+
+	const handleUpdateGuest = async () => {
+		const guestToken = uuid.v4()
+		if (guestToken) await handleCreateGuestUser({ guest_token: guestToken })
+	}
 
 	return (
 		<View style={styles.container}>
@@ -47,6 +73,7 @@ export const Home = (props: any) => {
 					isCircle={false}
 					onClick={() => onNavigationRedirect('Login')}
 					imgRightSrc={null}
+					isDisabled={orderState?.loading}
 				/>
 				<OButton
 					text={t('SIGNUP', 'Signup')}
@@ -56,15 +83,13 @@ export const Home = (props: any) => {
 					textStyle={{ color: 'black' }}
 					onClick={() => onNavigationRedirect('Signup')}
 					imgRightSrc={null}
+					isDisabled={orderState?.loading}
 				/>
 				<TouchableOpacity
+					disabled={orderState?.loading}
 					style={{ ...styles.textLink, marginTop: 12 }}
-					onPress={() =>
-						orderState?.options?.address?.address || isAllowUnaddressOrderType
-							? onNavigationRedirect(!!businessSlug ? 'Business' : 'BusinessList', { isGuestUser: true })
-							: onNavigationRedirect('AddressForm', { isGuestUser: true })
-					}>
-					<OText weight="normal" size={18} color={theme.colors.white}>
+					onPress={() => handleUpdateGuest()}>
+					<OText weight="normal" size={18} color={orderState?.loading ? '#ccc' : theme.colors.white}>
 						{t('CONTINUE_AS_GUEST', 'Continue as guest')}
 					</OText>
 				</TouchableOpacity>
