9327c08b8db47e110a8c86f68016b671a91b9067
Carlos Quiroz <77166972+carlosq-ordering@users.noreply.github.com>
2024-06-04 23:26:25 -0400

    Merge pull request #1776 from Ordering-Inc/fix-crash-delivery-app
    [FEAT] fix-crash-delivery-app

--- example/themes/business/src/components/MapView/index.tsx
+++ example/themes/business/src/components/MapView/index.tsx
@@ -24,6 +24,7 @@ const MapViewComponent = (props: MapViewParams) => {
     setDriverLocation,
     onNavigationRedirect,
     getBusinessLocations,
+    isDeliveryApp
   } = props;
 
   const theme = useTheme();
@@ -177,7 +178,7 @@ const MapViewComponent = (props: MapViewParams) => {
             longitude: coordinateLng
           })
         }
-        ref={(ref) => markerRef.current = ref}
+        ref={(ref: any) => markerRef.current = ref}
       >
         <Icon
           name="map-marker"
@@ -248,7 +249,7 @@ const MapViewComponent = (props: MapViewParams) => {
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <View style={{ flex: 1 }}>
-        {!isLoadingBusinessMarkers && isFocused && (
+        {(isDeliveryApp || (!isLoadingBusinessMarkers && isFocused)) && (
           <View style={{ flex: 1 }}>
             <MapView
               ref={mapRef}
