937af20ee9a846a0a018dc833aacfd4db2a45aec
Carlos Quiroz <77166972+carlosq-ordering@users.noreply.github.com>
2024-09-23 23:24:56 -0400

    Merge pull request #1793 from Ordering-Inc/implement-help-page
    Refactor: Remove unused HelpOrder, HelpAccountAndPayment, and HelpGui…

--- example/themes/original/src/components/Help/index.tsx
+++ example/themes/original/src/components/Help/index.tsx
@@ -1,26 +1,47 @@
 import React, { useState } from 'react'
-import { useTheme } from 'styled-components/native';
-import { Platform, RefreshControl } from 'react-native'
-import { HelpParams } from '../../types'
+import { Platform, RefreshControl, View } from 'react-native'
+import { Fade, Placeholder, PlaceholderLine } from 'rn-placeholder'
+import { useTheme } from 'styled-components/native'
 import { useLanguage } from 'ordering-components/native'
-import NavBar from '../NavBar'
-import { OText } from '../shared'
-import { LastOrders } from '../LastOrders'
+
+import { HelpParams } from '../../types'
+import { CmsContent } from './functions'
+import { HelpSubItem, LastOrdersContainer } from './styles'
 import { Container } from '../../layouts/Container'
 
-import {
-  HelpSubItem,
-  LastOrdersContainer
-} from './styles'
+import { OText, OIcon } from '../shared'
+import { NotFoundSource } from '../NotFoundSource'
+import NavBar from '../NavBar'
+import { LastOrders } from '../LastOrders'
 
-export const Help = (props: HelpParams) => {
-  const {
-    navigation
-  } = props
+const HelpUI = (props: HelpParams) => {
+  const { cmsState, navigation } = props
+
+  const { items, loading, error, pages } = cmsState
+
   const [, t] = useLanguage()
   const theme = useTheme()
-  const [refreshing] = useState(false);
+
+  const [refreshing] = useState(false)
   const [refresh, setRefresh] = useState(false)
+
+  const HelpItem = ({ item }: any) => {
+    const currentItem = pages.find((p: any) => p.id === item.id)
+    return (
+      <HelpSubItem
+        activeOpacity={0.7}
+        onPress={() => onRedirect('HelpOptions', { item: currentItem })}
+      >
+        <OText size={14} style={{ width: '90%' }}>
+          {item.name}
+        </OText>
+        <OIcon
+          width={15}
+          src={theme.images.general.chevron_right}
+        />
+      </HelpSubItem>
+    )
+  }
 
   const goToBack = () => navigation?.canGoBack() && navigation.goBack()
   const onRedirect = (route: string, params?: any) => {
@@ -49,26 +70,50 @@ export const Help = (props: HelpParams) => {
         showCall={false}
         btnStyle={{ paddingLeft: 0 }}
       />
-      <HelpSubItem
-        onPress={() => onRedirect('HelpOrder')}
+      <OText
+        size={20}
+        weight={'bold'}
+        style={{ marginBottom: 20 }}
       >
-        <OText size={14}>{t('HELP_WITH_ORDER', 'Help with an order')}</OText>
-      </HelpSubItem>
-      <HelpSubItem
-        onPress={() => onRedirect('HelpAccountAndPayment')}
-      >
-        <OText size={14}>{t('ACCOUNT_PAYMENT_OPTIONS', 'Account and Payment Options')}</OText>
-      </HelpSubItem>
-      <HelpSubItem
-        onPress={() => onRedirect('HelpGuide')}
-      >
-        <OText size={14}>{t('GUIDE_TO_ORDERING', 'Guide to Ordering')}</OText>
-      </HelpSubItem>
-
+        {t('ALL_THE_THEMES', 'All the themes')}
+      </OText>
+      {!loading && !error && items.map((item: any) => (
+        <HelpItem
+          key={item.id}
+          item={item}
+        />
+      ))}
+      {loading && (
+        <Placeholder Animation={Fade}>
+          <View style={{ flexDirection: 'column' }}>
+            {[...Array(5)].map((_, i) => (
+              <HelpSubItem key={i}>
+                <PlaceholderLine key={i} height={30} width={90} noMargin />
+              </HelpSubItem>
+            ))}
+          </View>
+        </Placeholder>
+      )}
+      {!loading && !!error && (
+        <NotFoundSource
+          simple
+          content={error}
+        />
+      )}
       <LastOrdersContainer>
         <OText size={18} weight={600}>{t('LAST_ORDERS', 'Last Orders')}</OText>
         <LastOrders {...props} onRedirect={onRedirect} refresh={refresh} setRefresh={setRefresh} />
       </LastOrdersContainer>
     </Container>
+  )
+}
+
+export const Help = (props: HelpParams) => {
+  const helpProps = {
+    ...props,
+    UIComponent: HelpUI
+  }
+  return (
+    <CmsContent {...helpProps} />
   )
 }
